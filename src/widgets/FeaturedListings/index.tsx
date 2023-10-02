ea2ca7155b0a7d732a74ca419f3d62010da0ce14
devcowsar <kawsarcse836@gmail.com>
2023-10-03 00:14:37 +0600

    fix: translation

--- src/widgets/FeaturedListings/index.tsx
+++ src/widgets/FeaturedListings/index.tsx
@@ -1,12 +1,19 @@
 import { FC } from "react";
-import { Grid } from "@mui/material";
+import { useTranslations } from "next-intl";
+import { Grid, Typography } from "@mui/material";
 
 import { FeaturedListingsProps } from "./Types";
 import Listing from "../Listing";
 
 const FeaturedListings: FC<FeaturedListingsProps> = () => {
+  const t = useTranslations();
   return (
     <Grid container spacing={5}>
+      <Grid item xs={12}>
+        <Typography variant="h3">
+          {t("sectionTitle.featuredProperties")}
+        </Typography>
+      </Grid>
       <Grid item xs={4}>
         <Listing />
       </Grid>
