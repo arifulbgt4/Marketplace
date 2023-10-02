1e1ede0beadc30cb30127fe8c1e2a1b3f620be33
devcowsar <kawsarcse836@gmail.com>
2023-10-03 00:14:37 +0600

    fix: translation

--- src/widgets/RecentListings/index.tsx
+++ src/widgets/RecentListings/index.tsx
@@ -1,12 +1,19 @@
 import { FC } from "react";
+import { useTranslations } from "next-intl";
+import { Grid, Typography } from "@mui/material";
 
 import { RecentListingsProps } from "./Types";
-import { Grid } from "@mui/material";
 import Listing from "../Listing";
 
 const RecentListings: FC<RecentListingsProps> = () => {
+  const t = useTranslations();
   return (
     <Grid container spacing={5} pt={1}>
+      <Grid item xs={12}>
+        <Typography variant="h3">
+          {t("sectionTitle.recentProperties")}
+        </Typography>
+      </Grid>
       <Grid item xs={4}>
         <Listing />
       </Grid>
