44198bf001f4ac3a0a7343c7666644633c4ab4d5
devcowsar <kawsarcse836@gmail.com>
2023-10-03 22:57:07 +0600

    feat:userlisting

--- src/app/[locale]/(User)/listing/page.tsx
+++ src/app/[locale]/(User)/listing/page.tsx
@@ -1,9 +1,13 @@
 import { Grid, Typography } from "@mui/material";
 
+import UserListing from "src/widgets/UserListing";
+
 const Listing = () => {
   return (
-    <Grid>
-      <Typography>Listing page</Typography>
+    <Grid container>
+      <Grid item xs={12}>
+        <UserListing />
+      </Grid>
     </Grid>
   );
 };
