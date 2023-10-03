54938b40dc6c91e9a217ce9a4487b66e676384f6
devcowsar <kawsarcse836@gmail.com>
2023-10-03 22:07:18 +0600

    upgrade:profie page feat user details social icon links

---
+++ src/widgets/UserDetails/index.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+import { Stack, Typography } from "@mui/material";
+
+import { UserDetailsProps } from "./Types";
+
+const UserDetails: FC<UserDetailsProps> = () => {
+  return (
+    <Stack>
+      <Stack direction="row">
+        <Typography>First Name</Typography>
+        <Typography>:abcd</Typography>
+      </Stack>
+      <Stack direction="row">
+        <Typography>Last name</Typography>
+        <Typography>:efgh</Typography>
+      </Stack>
+      <Stack direction="row">
+        <Typography>Adress</Typography>
+        <Typography>adress</Typography>
+      </Stack>
+      <Stack direction="row">
+        <Typography>Email Adress</Typography>
+        <Typography>:@mail</Typography>
+      </Stack>
+      <Stack direction="row">
+        <Typography>Phone Number</Typography>
+        <Typography>:012433</Typography>
+      </Stack>
+      <Stack direction="row">
+        <Typography>Phone Number</Typography>
+        <Typography>:012433</Typography>
+      </Stack>
+      <Stack direction="row">
+        <Typography>Age</Typography>
+        <Typography>:33</Typography>
+      </Stack>
+      <Stack direction="row">
+        <Typography>Gender</Typography>
+        <Typography>:male</Typography>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default UserDetails;
