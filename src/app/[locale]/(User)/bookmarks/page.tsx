72a363280bd5bc5b0add18649df80b07fb563b7e
devcowsar <kawsarcse836@gmail.com>
2023-10-03 19:26:31 +0600

    feat: user layout

---
+++ src/app/[locale]/(User)/bookmarks/page.tsx
@@ -0,0 +1,7 @@
+import { Grid } from "@mui/material";
+
+const Bookmarks = () => {
+  return <Grid>Bookmarks</Grid>;
+};
+
+export default Bookmarks;
---
+++ src/app/[locale]/(User)/profile/page.tsx
@@ -0,0 +1,5 @@
+import { Grid } from "@mui/material";
+
+export default function Profile() {
+  return <Grid>profile page</Grid>;
+}
