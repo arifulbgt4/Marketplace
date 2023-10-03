6c240a717215d235f06992ac8ec566ec89101d22
devcowsar <kawsarcse836@gmail.com>
2023-10-04 04:19:29 +0600

    feat:Search propertybanner listing propertybanner

--- src/app/[locale]/(User)/listing/page.tsx
+++ src/app/[locale]/(User)/listing/page.tsx
@@ -1,12 +1,23 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Box } from "@mui/material";
 
+import ListingPropertyBanner from "src/widgets/ListingPropertyBanner";
+import SearchProperty from "src/widgets/SearchProperty";
 import UserListing from "src/widgets/UserListing";
 
 const Listing = () => {
   return (
     <Grid container>
-      <Grid item xs={12}>
+      <Grid item xs={8}>
         <UserListing />
+      </Grid>
+      <Grid item xs={4}>
+        <Box>
+          <Typography variant="h4">Search Properties</Typography>
+          <SearchProperty />
+        </Box>
+        <Box mt={3}>
+          <ListingPropertyBanner />
+        </Box>
       </Grid>
     </Grid>
   );
