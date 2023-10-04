1cbd1ed70dbc8ad1d638a08166bfc1b3d7b528dd
devcowsar <kawsarcse836@gmail.com>
2023-10-04 20:00:16 +0600

    upgade:boomarkpage setting page components

--- src/app/[locale]/(User)/settings/page.tsx
+++ src/app/[locale]/(User)/settings/page.tsx
@@ -1,7 +1,19 @@
 import { Grid } from "@mui/material";
 
+import UserSettingForm from "src/widgets/UserSettingForm";
+import UserSettingItem from "src/widgets/UserSettingItem";
+
 const Settings = () => {
-  return <Grid>Settings</Grid>;
+  return (
+    <Grid container>
+      <Grid item xs={8}>
+        <UserSettingForm />
+      </Grid>
+      <Grid item xs={4}>
+        <UserSettingItem />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Settings;
---
+++ src/widgets/UserSettingForm/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Paper } from "@mui/material";
+
+import { UserSettingFormProps } from "./Types";
+
+const UserSettingForm: FC<UserSettingFormProps> = () => {
+  return <Paper>UserSettingForm</Paper>;
+};
+
+export default UserSettingForm;
