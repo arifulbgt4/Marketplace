dfb9d7ad7dccdac4dbfefbc7cf5f0659864bfda7
devcowsar <kawsarcse836@gmail.com>
2023-10-05 02:38:59 +0600

    feat: listing search catagory filter

--- src/app/[locale]/(ListingPages)/listingall/page.tsx
+++ src/app/[locale]/(ListingPages)/listingall/page.tsx
@@ -1,15 +1,32 @@
-import { Grid } from "@mui/material";
+import { Grid, Box, Typography, Stack } from "@mui/material";
 
+import Listings from "src/widgets/Listings";
+import SearchCategoryFilter from "src/widgets/SearchCategoryFilter";
 import SearchFilter from "src/widgets/SearchFilter";
+import SearchFilterForm from "src/widgets/SearchFilterForm";
 
 const ListingAll = () => {
   return (
     <Grid container>
-      <Grid item xs={4}>
-        searfilteritem
-      </Grid>
-      <Grid item xs={8}>
+      <Grid item xs={12}>
         <SearchFilter />
+      </Grid>
+      <Grid item xs={12} container>
+        <Grid item xs={4}>
+          <Stack direction="column" justifyContent="space-evenly">
+            <Box>
+              <Typography variant="h4">Categories</Typography>
+              <SearchCategoryFilter />
+            </Box>
+            <Box>
+              <SearchFilterForm />
+            </Box>
+          </Stack>
+        </Grid>
+        <Grid item xs={8}>
+          <Typography variant="h4">WE Found 91 Results</Typography>
+          <Listings />
+        </Grid>
       </Grid>
     </Grid>
   );
---
+++ src/widgets/SearchCategoryFilter/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Paper } from "@mui/material";
+
+import { SearchCategoryFilterProps } from "./Types";
+
+const SearchCategoryFilter: FC<SearchCategoryFilterProps> = () => {
+  return <Paper>SearchCategoryFilter</Paper>;
+};
+
+export default SearchCategoryFilter;
