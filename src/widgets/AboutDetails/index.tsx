1fa0accf9ac58c687ea211792a6bf7ef808b0281
devcowsar <kawsarcse836@gmail.com>
2023-10-05 03:58:08 +0600

    feat:about details

---
+++ src/widgets/AboutDetails/index.tsx
@@ -0,0 +1,10 @@
+import { Paper } from "@mui/material";
+import { FC } from "react";
+
+import { AboutDetailsProps } from "./Types";
+
+const AboutDetails: FC<AboutDetailsProps> = () => {
+  return <Paper>AboutDetails</Paper>;
+};
+
+export default AboutDetails;
