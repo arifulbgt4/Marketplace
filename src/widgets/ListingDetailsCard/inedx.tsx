fd73417a5f64f9a6d331cea6b0d167f2d3852941
devcowsar <kawsarcse836@gmail.com>
2023-10-05 03:28:04 +0600

    feat :deatailspage litsitngdetailscard

---
+++ src/widgets/ListingDetailsCard/inedx.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Card } from "@mui/material";
+
+import { ListingDetailsCardProps } from "./Types";
+
+const ListingDetailsCard: FC<ListingDetailsCardProps> = () => {
+  return <Card>ListingDetailsCard</Card>;
+};
+
+export default ListingDetailsCard;
