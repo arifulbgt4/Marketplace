74ade7104cf3d3e5d79323f25f7f530e41bd0b95
devcowsar <kawsarcse836@gmail.com>
2023-10-06 01:01:57 +0600

    feat:usermedia usermediaform

---
+++ src/widgets/UserMediaForm/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { FormControl, Paper } from "@mui/material";
+
+import { UserMediaFormProps } from "./Types";
+
+const UserMediaForm: FC<UserMediaFormProps> = () => {
+  return (
+    <Paper>
+      <FormControl>UserMediaForm</FormControl>
+    </Paper>
+  );
+};
+
+export default UserMediaForm;
