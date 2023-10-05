e8a1ec07e0f49eedaa3a06a22af441ed004cfb5c
devcowsar <kawsarcse836@gmail.com>
2023-10-05 22:19:39 +0600

    fix: import searchfilterform

--- src/widgets/SearchFilter/index.tsx
+++ src/widgets/SearchFilter/index.tsx
@@ -2,9 +2,14 @@ import { Paper } from "@mui/material";
 import { FC } from "react";
 
 import { SearchFilterProps } from "./Types";
+import SearchFilterForm from "src/forms/SearchFilterForm";
 
 const SearchFilter: FC<SearchFilterProps> = () => {
-  return <Paper>SearchFilterForm</Paper>;
+  return (
+    <Paper>
+      <SearchFilterForm />
+    </Paper>
+  );
 };
 
 export default SearchFilter;
