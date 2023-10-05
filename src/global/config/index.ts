e9b825bda72cab9c3865ba0278d35ccefda5a1d0
Ariful islam <arifulbgt4@gmail.com>
2023-10-05 22:12:38 +0600

    feat: seo config

---
+++ src/global/config/index.ts
@@ -0,0 +1,30 @@
+const siteAddress =
+  process.env.NEXT_PUBLIC_SITE_ADDRESS || "http://localhost:3000/";
+
+export const siteConfig = {
+  name: "B2C Marketplace System",
+  shortName: "B2C Marketplace",
+  url: siteAddress,
+  ogImage: `${siteAddress}opengraph-image`,
+  description:
+    "It provides a frontend, backend, and database schema that startups can use to create their own B2C applications with a few simple changes. This can help startups to get a head start on their development and to launch their applications more quickly and affordably",
+  links: {
+    github: "https://github.com/arifulbgt4/Marketplace",
+  },
+  keywords: [
+    "Frontend Engineer",
+    "JavaScript Develope",
+    "React.js Specialis",
+    "Next.js Expert",
+    "Senior Web Developer",
+    "User-Centric Design",
+    "UI/UX Enthusiast",
+    "Node.js Developer",
+    "Web Application Architect",
+    "Code Refactoring Pro",
+  ],
+  author: "Ariful",
+  creator: "Ariful",
+  themeColor: "dark",
+  backgroundColor: "#090c24",
+};
