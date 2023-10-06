fc6553b314fd48c82c1a987e87f7952c68bb6954
devcowsar <kawsarcse836@gmail.com>
2023-10-06 16:36:05 +0600

    upgrade: header

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -1,9 +1,146 @@
-import { FC } from "react";
+"use client";
+import { FC, useState } from "react";
+import {
+  AppBar,
+  Container,
+  SvgIcon,
+  Toolbar,
+  Typography,
+  Box,
+  IconButton,
+  MenuItem,
+  Button,
+  Tooltip,
+  Menu,
+  Avatar,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
 import { HeaderProps } from "./Types";
-import { AppBar } from "@mui/material";
+import Logo from "../Logo";
 
+const pages = ["All Properties", "Start selling", "Abou us", "Contact us"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 const Header: FC<HeaderProps> = () => {
-  return <AppBar position="sticky">Header</AppBar>;
-};
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
 
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box
+            flexGrow={{ xs: 0, md: 1 }}
+            sx={{
+              display: { xs: "none", md: "flex", alignItems: "center" },
+              mr: 1,
+            }}
+          >
+            <Logo />
+          </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              alignItems: "end",
+              display: { xs: "flex", md: "none" },
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}>
+            <Logo />
+          </Box>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Button onClick={handleOpenUserMenu}>Create Account</Button>
+            <Button sx={{ display: { xs: "none", md: "inline-block" } }}>
+              Create Account
+            </Button>
+            {/* <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>*/}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 export default Header;
