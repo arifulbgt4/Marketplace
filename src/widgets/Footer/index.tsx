2e4d121026ab46b693bfa7483805c9b4026f729c
devcowsar <kawsarcse836@gmail.com>
2023-10-07 15:29:25 -0700

    upagrade:footer

--- src/widgets/Footer/index.tsx
+++ src/widgets/Footer/index.tsx
@@ -1,15 +1,51 @@
 import { FC } from "react";
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Stack, Typography } from "@mui/material";
 
 import { FooterProps } from "./Types";
+import SocialIconLink from "../SocialIconLink";
+import Logo from "src/components/Logo";
 
 const Footer: FC<FooterProps> = () => {
   return (
     <Box>
       <Container>
-        <Grid container>
+        <Grid container gap={10} pt={15} pb={10}>
           <Grid item xs={12}>
-            <Typography>@copyright 2023</Typography>
+            <Stack direction="row" justifyContent="space-between" gap={24}>
+              <Stack gap={2}>
+                <Typography variant="h6">PRIVACY POLICY</Typography>
+                <Typography>
+                  Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+                  odit aut fugit, sed quia consequuntur ma
+                </Typography>
+                <Typography> Louis Vuitton</Typography>
+              </Stack>
+              <Stack gap={2}>
+                <Typography variant="h6">LOCATION</Typography>
+                <Typography>
+                  2972 Westheimer Rd. Santa Ana, Illinois 85486
+                </Typography>
+              </Stack>
+              <Stack gap={2}>
+                <Typography variant="h6">CONTACT US</Typography>
+                <Typography>michelle.rivera@example.com</Typography>
+                <Typography> (480) 555-0103</Typography>
+              </Stack>
+              <Stack gap={2}>
+                <Typography variant="h6">COMMUNITIES</Typography>
+                <Typography>
+                  3517 W. Gray St. Utica, Pennsylvania 57867
+                </Typography>
+                <Typography> (217) 555-0113</Typography>
+                <Typography>Acme Co.</Typography>
+              </Stack>
+            </Stack>
+          </Grid>
+          <Grid item xs={12}>
+            <Stack direction="column" alignItems="center" gap={4}>
+              <SocialIconLink />
+              <Logo />
+            </Stack>
           </Grid>
         </Grid>
       </Container>
--- src/widgets/SocialIconLink/index.tsx
+++ src/widgets/SocialIconLink/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { FC } from "react";
-import { Stack, Typography, Paper, Link } from "@mui/material";
+import { Stack, Typography, Paper, Link, Box } from "@mui/material";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
@@ -8,30 +8,25 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 import { SocialIconLinkProps } from "./Types";
 
+const linksicon = [
+  { icon: <TwitterIcon sx={{ width: 32, height: 26 }} />, link: "#" },
+  { icon: <FacebookIcon sx={{ width: 32, height: 26 }} />, link: "#" },
+  { icon: <InstagramIcon sx={{ width: 32, height: 26 }} />, link: "#" },
+  { icon: <LinkedInIcon sx={{ width: 32, height: 26 }} />, link: "#" },
+];
+
 const SocialIconLink: FC<SocialIconLinkProps> = () => {
   return (
     <Paper>
-      <Stack direction="row">
-        <Link href="#">
-          <Typography>
-            <TwitterIcon />
-          </Typography>
-        </Link>
-        <Link href="#">
-          <Typography>
-            <FacebookIcon />
-          </Typography>
-        </Link>
-        <Link href="#">
-          <Typography>
-            <InstagramIcon />
-          </Typography>
-        </Link>
-        <Link href="#">
-          <Typography>
-            <LinkedInIcon />
-          </Typography>
-        </Link>
+      <Stack direction="row" gap={5}>
+        {linksicon.map((data) => {
+          const { link, icon } = data;
+          return (
+            <Box component={Link} href={link}>
+              <Typography>{icon}</Typography>
+            </Box>
+          );
+        })}
       </Stack>
     </Paper>
   );
