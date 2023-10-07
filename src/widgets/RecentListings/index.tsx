81fa8054826329ce77adbbfb3d3bae614d25d06b
jueal hossain <mdjueal920977@gmail.com>
2023-10-07 19:02:33 +0600

    fix: FeaturedListings and RecentListings

--- src/widgets/RecentListings/index.tsx
+++ src/widgets/RecentListings/index.tsx
@@ -1,39 +1,42 @@
 import { FC } from "react";
 import { useTranslations } from "next-intl";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
 
 import Listing from "src/widgets/Listing";
+import { siteConfig } from "src/global/config";
 
 import { RecentListingsProps } from "./Types";
 
 const RecentListings: FC<RecentListingsProps> = () => {
   const t = useTranslations();
   return (
-    <Grid container spacing={5} pt={1}>
-      <Grid item xs={12}>
-        <Typography variant="h3">
-          {t("sectionTitle.recentProperties")}
-        </Typography>
+    <Stack alignItems="center">
+      <Grid container spacing={5} pt={1} maxWidth={siteConfig.maxWidth}>
+        <Grid item xs={12}>
+          <Typography variant="h3">
+            {t("sectionTitle.recentProperties")}
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Listing />
+        </Grid>
+        <Grid item xs={4}>
+          <Listing />
+        </Grid>
+        <Grid item xs={4}>
+          <Listing />
+        </Grid>
+        <Grid item xs={4}>
+          <Listing />
+        </Grid>
+        <Grid item xs={4}>
+          <Listing />
+        </Grid>
+        <Grid item xs={4}>
+          <Listing />
+        </Grid>
       </Grid>
-      <Grid item xs={4}>
-        <Listing />
-      </Grid>
-      <Grid item xs={4}>
-        <Listing />
-      </Grid>
-      <Grid item xs={4}>
-        <Listing />
-      </Grid>
-      <Grid item xs={4}>
-        <Listing />
-      </Grid>
-      <Grid item xs={4}>
-        <Listing />
-      </Grid>
-      <Grid item xs={4}>
-        <Listing />
-      </Grid>
-    </Grid>
+    </Stack>
   );
 };
 
