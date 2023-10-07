80841b33ca34082e8add7583e2f21f461391d57a
jueal hossain <mdjueal920977@gmail.com>
2023-10-07 18:56:50 +0600

    fix: footer and featurelistings

--- src/widgets/Footer/index.tsx
+++ src/widgets/Footer/index.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import { Grid, Stack, Typography } from "@mui/material";
 
-import { FooterProps } from "./Types";
 import { siteConfig } from "src/global/config";
+
+import { FooterProps } from "./Types";
 
 const Footer: FC<FooterProps> = () => {
   return (
-    <Stack justifyContent="center" alignItems="center">
+    <Stack justifyContent="center">
       <Grid container maxWidth={siteConfig.maxWidth}>
         <Grid item xs={12}>
           <Typography>@copyright 2023</Typography>
