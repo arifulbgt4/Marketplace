a16e3acbf84e35645513dcdb3233b52226de6d71
devcowsar <kawsarcse836@gmail.com>
2023-10-07 12:22:36 -0700

    fix:format

--- src/widgets/RecentListings/index.tsx
+++ src/widgets/RecentListings/index.tsx
@@ -10,33 +10,32 @@ import { RecentListingsProps } from "./Types";
 const RecentListings: FC<RecentListingsProps> = () => {
   const t = useTranslations();
   return (
-<Box>
+    <Box>
       <Container>
-    <Grid container spacing={5} pt={1}>
-      <Grid item xs={12}>
-        <Typography variant="h3">
-          {t("sectionTitle.recentProperties")}
-        </Typography>
-      </Grid>
-
-      {listings.map((data) => {
-        const { id, image, title, price, description, rating, slug } = data;
-        return (
-          <Grid item xs={4} key={id}>
-            <Listing
-              id={id}
-              slug={slug}
-              image={image}
-              title={title}
-              price={price}
-              description={description}
-              rating={rating}
-            />
+        <Grid container spacing={5} pt={1}>
+          <Grid item xs={12}>
+            <Typography variant="h3">
+              {t("sectionTitle.recentProperties")}
+            </Typography>
           </Grid>
-        );
-      })}
-    </Grid>
-        
+
+          {listings.map((data) => {
+            const { id, image, title, price, description, rating, slug } = data;
+            return (
+              <Grid item xs={4} key={id}>
+                <Listing
+                  id={id}
+                  slug={slug}
+                  image={image}
+                  title={title}
+                  price={price}
+                  description={description}
+                  rating={rating}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
       </Container>
     </Box>
   );
