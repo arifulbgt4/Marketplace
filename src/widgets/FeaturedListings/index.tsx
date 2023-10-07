92db17ef21428dce75291f00917ab877300ea84d
jueal hossain <mdjueal920977@gmail.com>
2023-10-07 20:05:05 +0600

    fix: FeaturedListings,RecentListings,Container and footer

--- src/widgets/FeaturedListings/index.tsx
+++ src/widgets/FeaturedListings/index.tsx
@@ -1,47 +1,47 @@
 import { FC } from "react";
 import { useTranslations } from "next-intl";
-import { Grid, Typography, Stack } from "@mui/material";
+import { Grid, Typography, Box, Container } from "@mui/material";
 
 import Listing from "src/widgets/Listing";
-import { siteConfig } from "src/global/config";
 
 import { FeaturedListingsProps } from "./Types";
 
 const FeaturedListings: FC<FeaturedListingsProps> = () => {
   const t = useTranslations();
   return (
-    <Stack alignItems="center">
-      <Grid
-        container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 4, sm: 8, md: 12 }}
-        maxWidth={siteConfig.maxWidth}
-      >
-        <Grid item xs={12}>
-          <Typography variant="h3">
-            {t("sectionTitle.featuredProperties")}
-          </Typography>
+    <Box>
+      <Container>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          <Grid item xs={12}>
+            <Typography variant="h3">
+              {t("sectionTitle.featuredProperties")}
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Listing />
+          </Grid>
+          <Grid item xs={4}>
+            <Listing />
+          </Grid>
+          <Grid item xs={4}>
+            <Listing />
+          </Grid>
+          <Grid item xs={4}>
+            <Listing />
+          </Grid>
+          <Grid item xs={4}>
+            <Listing />
+          </Grid>
+          <Grid item xs={4}>
+            <Listing />
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <Listing />
-        </Grid>
-        <Grid item xs={4}>
-          <Listing />
-        </Grid>
-        <Grid item xs={4}>
-          <Listing />
-        </Grid>
-        <Grid item xs={4}>
-          <Listing />
-        </Grid>
-        <Grid item xs={4}>
-          <Listing />
-        </Grid>
-        <Grid item xs={4}>
-          <Listing />
-        </Grid>
-      </Grid>
-    </Stack>
+      </Container>
+    </Box>
   );
 };
 
