b19f799a78171089475dc82e08962eea242179c1
devcowsar <kawsarcse836@gmail.com>
2023-10-08 07:12:48 -0700

    fix:section spacing

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,7 +8,7 @@ import RecentListings from "src/widgets/RecentListings";
 export default function Home() {
   // const t = useTranslations();
   return (
-    <Grid container>
+    <Grid container spacing={20}>
       <Grid item xs={12}>
         <SearchBanner />
       </Grid>
