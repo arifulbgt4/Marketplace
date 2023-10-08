b15a5cd912e68fcb9e55527ff61f147f17a2942a
devcowsar <kawsarcse836@gmail.com>
2023-10-08 15:03:37 -0700

    fix: signininfo signup info

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -1,14 +1,16 @@
 import { FC } from "react";
-import { Stack, Typography, Box } from "@mui/material";
+import { Stack, Typography, Box, Paper } from "@mui/material";
 
 import { InfoProps } from "./Types";
 
 const Info: FC<InfoProps> = ({ title, children }) => {
   return (
-    <Stack>
-      <Typography>{title}</Typography>
+    <Paper elevation={11}>
+      <Box py={3} pl={10} bgcolor="inherit">
+        <Typography variant="h4">{title}</Typography>
+      </Box>
       <Box>{children}</Box>
-    </Stack>
+    </Paper>
   );
 };
 
--- src/widgets/SignupInfo/index.tsx
+++ src/widgets/SignupInfo/index.tsx
@@ -1,5 +1,11 @@
+"use client";
 import { FC } from "react";
-import { Paper, Stack, MenuItem } from "@mui/material";
+import { Paper, Stack, Typography, Button } from "@mui/material";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem, { timelineItemClasses } from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
 
 import Info from "src/components/Info";
 
@@ -7,13 +13,51 @@ import { SignupInfoProps } from "./Types";
 
 const SignupInfo: FC<SignupInfoProps> = () => {
   return (
-    <Stack>
-      <Paper>
-        <Info title="Why Sign Up?">
-          <MenuItem>recent properties</MenuItem>
-        </Info>
-      </Paper>
-    </Stack>
+    <Paper>
+      <Info title="Why Sign Up?">
+        <Stack py={7.5} px={10} gap={5} alignItems="flex-start">
+          <Typography variant="h6">
+            Registering for an account is free and gives you access to:
+          </Typography>
+          <Timeline
+            sx={{
+              [`& .${timelineItemClasses.root}:before`]: {
+                flex: 0,
+                padding: 0,
+              },
+              [`& .${timelineItemClasses.root}`]: {
+                flex: 0,
+                px: 7.5,
+                minHeight: 50,
+              },
+            }}
+          >
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+              </TimelineSeparator>
+              <TimelineContent>Rent properties</TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+              </TimelineSeparator>
+              <TimelineContent>Listing properties</TimelineContent>
+            </TimelineItem>
+
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+              </TimelineSeparator>
+              <TimelineContent>Custom profile page</TimelineContent>
+            </TimelineItem>
+          </Timeline>
+          <Button size="large" variant="outlined">
+            Sign Up Now
+          </Button>
+        </Stack>
+      </Info>
+    </Paper>
   );
 };
 
