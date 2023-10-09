fe097f9a064b318be4ed54e20fef55142b3dd1c3
jueal hossain <mdjueal920977@gmail.com>
2023-10-10 03:58:29 +0600

    fix: locationMap and DashboardHelpInfo

--- src/widgets/DashboardHelpInfo/index.tsx
+++ src/widgets/DashboardHelpInfo/index.tsx
@@ -1,13 +1,47 @@
-import { FC } from "react";
+"use client";
+import { FC, useState } from "react";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Stack,
+  Button,
+  Typography,
+} from "@mui/material";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+import Info from "src/components/Info";
 
 import { DashboardHelpInfoProps } from "./Types";
-import Info from "src/components/Info";
-import BannerCreateListing from "../BannerCreateListing";
 
 const DashboardHelpInfo: FC<DashboardHelpInfoProps> = () => {
+  const [label, setLabel] = useState("");
+  const handleChange = (event: SelectChangeEvent) => {
+    setLabel(event.target.value as string);
+  };
   return (
     <Info title="Begin Upload">
-      <BannerCreateListing />
+      <Typography variant="h5">Select a category for your upload:</Typography>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-filled-label">Label</InputLabel>
+        <Select
+          labelId="demo-simple-select-filled-label"
+          id="demo-simple-select-filled"
+          value={label}
+          onChange={handleChange}
+        >
+          <MenuItem value="label">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={1}>One</MenuItem>
+          <MenuItem value={2}>Two</MenuItem>
+          <MenuItem value={3}>Three</MenuItem>
+        </Select>
+      </FormControl>
+      <Stack direction="row" justifyContent="space-between">
+        <Button>UPLOAD</Button>
+        <Button>READ INSTRUCTIONS</Button>
+      </Stack>
     </Info>
   );
 };
--- src/widgets/LocationsMap/index.tsx
+++ src/widgets/LocationsMap/index.tsx
@@ -1,7 +1,8 @@
 import { FC } from "react";
 
-import { LocationsMapProps } from "./Types";
 import Info from "src/components/Info";
+
+import { LocationsMapProps } from "./Types";
 
 const LocationsMap: FC<LocationsMapProps> = () => {
   return <Info title="Location on Map">Map</Info>;
