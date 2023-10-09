86a87cc19b95fd7f11bf6ff013c8a78e057f0eaa
devcowsar <kawsarcse836@gmail.com>
2023-10-09 05:13:36 -0700

    upgrade: authpages

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -1,12 +1,12 @@
 import { FC } from "react";
-import { Stack, Typography, Box, Paper } from "@mui/material";
+import { Typography, Box, Paper } from "@mui/material";
 
 import { InfoProps } from "./Types";
 
 const Info: FC<InfoProps> = ({ title, children }) => {
   return (
     <Paper elevation={11}>
-      <Box py={3} pl={10} bgcolor="inherit">
+      <Box py={3} pl={7} bgcolor="inherit">
         <Typography variant="h4">{title}</Typography>
       </Box>
       <Box>{children}</Box>
--- src/widgets/SignupInfo/index.tsx
+++ src/widgets/SignupInfo/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { FC } from "react";
-import { Paper, Stack, Typography, Button } from "@mui/material";
+import { Paper, Stack, Typography, Button, Box } from "@mui/material";
 import Timeline from "@mui/lab/Timeline";
 import TimelineItem, { timelineItemClasses } from "@mui/lab/TimelineItem";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
@@ -15,47 +15,50 @@ const SignupInfo: FC<SignupInfoProps> = () => {
   return (
     <Paper>
       <Info title="Why Sign Up?">
-        <Stack py={7.5} px={10} gap={5} alignItems="flex-start">
-          <Typography variant="h6">
-            Registering for an account is free and gives you access to:
-          </Typography>
-          <Timeline
-            sx={{
-              [`& .${timelineItemClasses.root}:before`]: {
-                flex: 0,
-                padding: 0,
-              },
-              [`& .${timelineItemClasses.root}`]: {
-                flex: 0,
-                px: 7.5,
-                minHeight: 50,
-              },
-            }}
-          >
-            <TimelineItem>
-              <TimelineSeparator>
-                <TimelineDot />
-              </TimelineSeparator>
-              <TimelineContent>Rent properties</TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              <TimelineSeparator>
-                <TimelineDot />
-              </TimelineSeparator>
-              <TimelineContent>Listing properties</TimelineContent>
-            </TimelineItem>
+        <Box px={7} py={5}>
+          <Stack gap={5} alignItems="flex-start">
+            <Typography variant="h6">
+              Registering for an account is free and gives you access to:
+            </Typography>
+            <Box pl={4}>
+              <Timeline
+                sx={{
+                  [`& .${timelineItemClasses.root}:before`]: {
+                    flex: 0,
+                    px: 0,
+                  },
+                  [`& .${timelineItemClasses.root}`]: {
+                    flex: 0,
+                    minHeight: 35,
+                  },
+                }}
+              >
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot />
+                  </TimelineSeparator>
+                  <TimelineContent>Rent properties</TimelineContent>
+                </TimelineItem>
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot />
+                  </TimelineSeparator>
+                  <TimelineContent>Listing properties</TimelineContent>
+                </TimelineItem>
 
-            <TimelineItem>
-              <TimelineSeparator>
-                <TimelineDot />
-              </TimelineSeparator>
-              <TimelineContent>Custom profile page</TimelineContent>
-            </TimelineItem>
-          </Timeline>
-          <Button size="large" variant="outlined">
-            Sign Up Now
-          </Button>
-        </Stack>
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot />
+                  </TimelineSeparator>
+                  <TimelineContent>Custom profile page</TimelineContent>
+                </TimelineItem>
+              </Timeline>
+            </Box>
+            <Button size="large" variant="outlined">
+              Sign Up Now
+            </Button>
+          </Stack>
+        </Box>
       </Info>
     </Paper>
   );
