ac5e24b0926dd69d3e27ad8207f825869b06f4b4
devcowsar <kawsarcse836@gmail.com>
2023-10-10 13:40:33 -0700

    fix:usersetting page

--- src/app/[locale]/u/setting/page.tsx
+++ src/app/[locale]/u/setting/page.tsx
@@ -1,15 +1,7 @@
-import { Grid } from "@mui/material";
-
 import UserSettingForm from "src/forms/UserSettingForm";
 
 const Settings = () => {
-  return (
-    <Grid container>
-      <Grid item xs={12}>
-        <UserSettingForm />
-      </Grid>
-    </Grid>
-  );
+  return <UserSettingForm />;
 };
 
 export default Settings;
