aa10370c2631fa12b81641bd480035036671ede5
jueal hossain <mdjueal920977@gmail.com>
2023-10-10 23:07:52 +0600

    fix: Applayout and header

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -1,16 +1,19 @@
 import { FC } from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "src/lib/auth";
 import { Grid } from "@mui/material";
 
-import { AppLayoutProps } from "./Types";
 import Header from "src/widgets/Header";
-import Navigation from "src/widgets/Navigation";
 import Footer from "src/widgets/Footer";
 
-const AppLayout: FC<AppLayoutProps> = ({ children }) => {
+import { AppLayoutProps } from "./Types";
+
+const AppLayout: FC<AppLayoutProps> = async ({ children }) => {
+  const session = await getServerSession(authOptions);
   return (
     <Grid container>
       <Grid item xs={12}>
-        <Header />
+        <Header user={session?.user} />
       </Grid>
       <Grid item xs={12}>
         {children}
