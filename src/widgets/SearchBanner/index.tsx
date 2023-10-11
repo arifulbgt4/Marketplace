8964e63d1faa0de047678acdde7f718a5f28a3cc
devcowsar <kawsarcse836@gmail.com>
2023-10-11 14:00:28 -0700

    fix: search filterform

--- src/widgets/SearchBanner/index.tsx
+++ src/widgets/SearchBanner/index.tsx
@@ -4,7 +4,6 @@ import { Typography, Box, Paper, Stack } from "@mui/material";
 import SearchFilterForm from "src/forms/SearchFilterForm";
 
 import { SearchBannerProps } from "./Types";
-import { listings } from "src/global/staticData";
 
 const bannaerimg =
   "https://scontent.fdac151-1.fna.fbcdn.net/v/t1.15752-9/384820780_347011921016699_8313359896565317700_n.png?_nc_cat=108&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeEsLQgGhu08CR-DeUfDtMCe5CjaR2Hn_ezkKNpHYef97N935-sm0XaD9SVIEYXyWpTvjQrW4un2e_UAfpl4a086&_nc_ohc=4FHVr7aI6sYAX-n0HLO&_nc_ht=scontent.fdac151-1.fna&oh=03_AdT75Hvg4NzRPcxDhqYD4xxuxvcPDBP9wr3zA80zkKOZ3A&oe=654771B8";
@@ -27,7 +26,9 @@ const SearchBanner: FC<SearchBannerProps> = () => {
     >
       <Box textAlign="center">
         <Typography variant="h2">Find Home Together</Typography>
-        <Typography variant="h4">
+      </Box>
+      <Box>
+        <Typography variant="h4" pb={2}>
           25,000 rooms amd houses available now on Dream House{" "}
         </Typography>
         <SearchFilterForm />
