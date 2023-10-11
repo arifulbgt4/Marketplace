0719a29671435d6c2b8dcc2b7a516e014b493a8e
devcowsar <kawsarcse836@gmail.com>
2023-10-11 04:49:23 -0700

    fix :setting forms

--- src/forms/SocialForm/index.tsx
+++ src/forms/SocialForm/index.tsx
@@ -29,62 +29,66 @@ const SocilalForm: FC<SocilalFormProps> = () => {
       initialValues={INITIAL_VALUES}
       render={({ handleSubmit, values, errors, submitting }) => {
         return (
-          <Grid container rowSpacing={5}>
-            <Grid item xs={2}>
-              <FacebookRoundedIcon sx={{ width: 40, height: 40 }} />
-            </Grid>
-            <Grid item xs={9}>
-              <TextField
-                name="facebook"
-                fullWidth
-                id="full-width"
-                label="Facebook.com/"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={2}>
-              <LinkedInIcon sx={{ width: 40, height: 40 }} />
-            </Grid>
-            <Grid item xs={9}>
-              <TextField
-                name="linkedIn"
-                fullWidth
-                id="full-width"
-                label="LinkedIn.com/"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={2}>
-              <InstagramIcon sx={{ width: 40, height: 40 }} />
-            </Grid>
-            <Grid item xs={9}>
-              <TextField
-                name="instragram"
-                fullWidth
-                id="full-width"
-                label="Instagram.com/"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={2}>
-              <TwitterIcon sx={{ width: 40, height: 40 }} />
-            </Grid>
-            <Grid item xs={9} container rowSpacing={2}>
-              <TextField
-                name="twitter"
-                fullWidth
-                id="full-width"
-                label="Twitter.com/"
-                variant="outlined"
-                required
-              />
-              <Grid item xs={12}>
-                <Button variant="outlined" type="submit">
-                  Update
-                </Button>
+          <form onSubmit={handleSubmit}>
+            <Grid container rowSpacing={5}>
+              <Grid item xs={2}>
+                <FacebookRoundedIcon sx={{ width: 40, height: 40 }} />
+              </Grid>
+              <Grid item xs={9}>
+                <TextField
+                  name="facebook"
+                  fullWidth
+                  id="full-width"
+                  label="Facebook.com/"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <LinkedInIcon sx={{ width: 40, height: 40 }} />
+              </Grid>
+              <Grid item xs={9}>
+                <TextField
+                  name="linkedIn"
+                  fullWidth
+                  id="full-width"
+                  label="LinkedIn.com/"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <InstagramIcon sx={{ width: 40, height: 40 }} />
+              </Grid>
+              <Grid item xs={9}>
+                <TextField
+                  name="instragram"
+                  fullWidth
+                  id="full-width"
+                  label="Instagram.com/"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <TwitterIcon sx={{ width: 40, height: 40 }} />
+              </Grid>
+              <Grid item xs={9} container rowSpacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    name="twitter"
+                    fullWidth
+                    id="full-width"
+                    label="Twitter.com/"
+                    variant="outlined"
+                    required
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button variant="outlined" type="submit">
+                    Update
+                  </Button>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
+          </form>
         );
       }}
     />
