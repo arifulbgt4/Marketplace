08c81c0f9d19a62991c2772e78b1c03d986bfb2b
jueal hossain <mdjueal920977@gmail.com>
2023-10-11 19:53:23 +0600

    fix: SearchPage

--- src/widgets/SearchListingGroup/index.tsx
+++ src/widgets/SearchListingGroup/index.tsx
@@ -1,9 +1,36 @@
 import { FC } from "react";
+import { Grid } from "@mui/material";
 
 import { SearchListingGroupProps } from "./Types";
 
 const SearchListingGroup: FC<SearchListingGroupProps> = () => {
-  return <div>SearchListingGroup</div>;
+  return (
+    <Grid container>
+      <Grid item container xs={12}>
+        <Grid item xs={4}>
+          Card
+        </Grid>
+        <Grid item xs={4}>
+          Card
+        </Grid>
+        <Grid item xs={4}>
+          Card
+        </Grid>
+        <Grid item xs={4}>
+          Card
+        </Grid>
+        <Grid item xs={4}>
+          Card
+        </Grid>
+        <Grid item xs={4}>
+          Card
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        ListingPagination
+      </Grid>
+    </Grid>
+  );
 };
 
 export default SearchListingGroup;
