683e62e432693c85031002005c9d519e90b6bfab
jueal hossain <mdjueal920977@gmail.com>
2023-10-11 19:21:33 +0600

    feat: Searchbar layout

--- src/app/[locale]/s/page.tsx
+++ src/app/[locale]/s/page.tsx
@@ -1,5 +1,22 @@
+import { Box, Container, Grid } from "@mui/material";
+
+import ListSearchFilters from "src/widgets/ListSearchFilters";
+
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <Box>
+      <Container>
+        <Grid container>
+          <Grid item xs={3}>
+            <ListSearchFilters />
+          </Grid>
+          <Grid item xs={9}>
+            SearchListingGroup
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 };
 
 export default SearchPage;
