73de22c7c9463dd7953f3b151ce1f682c6468dd2
devcowsar <kawsarcse836@gmail.com>
2023-10-11 11:06:37 -0700

    fix: search filter form

--- src/forms/SearchFilterForm/index.tsx
+++ src/forms/SearchFilterForm/index.tsx
@@ -1,24 +1,74 @@
 "use client";
 import { FC, useState } from "react";
-import { Autocomplete, Paper, FormControl, TextField } from "@mui/material";
+import {
+  Paper,
+  FormControl,
+  FilledInput,
+  Button,
+  Grid,
+  InputLabel,
+  InputAdornment,
+} from "@mui/material";
+import SearchSharpIcon from "@mui/icons-material/SearchSharp";
+import LocationOnSharpIcon from "@mui/icons-material/LocationOnSharp";
+import IconButton from "@mui/material/IconButton";
+
+import { Form as FinalForm } from "react-final-form";
 
 import { SearchFilterFormProps } from "./Types";
-import { listings } from "src/global/staticData";
 
 const SearchFilterForm: FC<SearchFilterFormProps> = ({}) => {
-  const searchingProps = {
-    options: listings.map((data) => data.title),
-  };
-  const [value, setValue] = useState(null);
+  const onSubmitForm = async () => {};
+
   return (
     <Paper>
-      <Autocomplete
-        {...searchingProps}
-        disablePortal
-        id="combo-box-demo"
-        renderInput={(params) => (
-          <TextField name="search" {...params} label="Search" />
-        )}
+      <FinalForm
+        onSubmit={onSubmitForm}
+        render={({ handleSubmit, values, errors, submitting }) => {
+          return (
+            <form onSubmit={handleSubmit}>
+              <Grid container>
+                <Grid item xs={4}>
+                  <FormControl sx={{ p: 1, width: "25ch" }} variant="filled">
+                    <InputLabel htmlFor="filled-adornment-location">
+                      Location
+                    </InputLabel>
+                    <FilledInput
+                      id="filled-adornment-location"
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton>
+                            <LocationOnSharpIcon />
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item xs={4}>
+                  <FormControl sx={{ m: 1, width: "25ch" }} variant="filled">
+                    <InputLabel htmlFor="filled-adornment-password">
+                      Key
+                    </InputLabel>
+                    <FilledInput
+                      id="filled-adornment-password"
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton>
+                            <SearchSharpIcon />
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item xs={4} pt={2}>
+                  <Button variant="contained">Search</Button>
+                </Grid>
+              </Grid>
+            </form>
+          );
+        }}
       />
     </Paper>
   );
