f0198aaaefe843befa1bba3994f6705a8a9a9333
jueal hossain <mdjueal920977@gmail.com>
2023-10-11 19:21:33 +0600

    feat: Searchbar layout

---
+++ src/widgets/ListSearchFilters/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import { ListSearchFiltersProps } from "./Types";
+
+const ListSearchFilters: FC<ListSearchFiltersProps> = () => {
+  return <div>ListSearchFilters</div>;
+};
+
+export default ListSearchFilters;
---
+++ src/widgets/SearchListingGroup/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import { SearchListingGroupProps } from "./Types";
+
+const SearchListingGroup: FC<SearchListingGroupProps> = () => {
+  return <div>SearchListingGroup</div>;
+};
+
+export default SearchListingGroup;
