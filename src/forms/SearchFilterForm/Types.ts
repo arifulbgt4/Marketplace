b603aa86b54dd6da50f8a44b085d36dd244c787c
devcowsar <kawsarcse836@gmail.com>
2023-10-11 07:24:48 -0700

    fix:seacrhfilterform

--- src/forms/SearchFilterForm/Types.ts
+++ src/forms/SearchFilterForm/Types.ts
@@ -1,10 +1,3 @@
 export interface SearchFilterFormProps {
     
-    id:number,
-    slug:string,
-    title:string,
-    description:string,
-    image:string
-    price:number
-    ratting:number
 }
--- src/widgets/SearchBanner/index.tsx
+++ src/widgets/SearchBanner/index.tsx
@@ -4,6 +4,7 @@ import { Typography, Box, Paper, Stack } from "@mui/material";
 import SearchFilterForm from "src/forms/SearchFilterForm";
 
 import { SearchBannerProps } from "./Types";
+import { listings } from "src/global/staticData";
 
 const bannaerimg =
   "https://scontent.fdac151-1.fna.fbcdn.net/v/t1.15752-9/384820780_347011921016699_8313359896565317700_n.png?_nc_cat=108&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeEsLQgGhu08CR-DeUfDtMCe5CjaR2Hn_ezkKNpHYef97N935-sm0XaD9SVIEYXyWpTvjQrW4un2e_UAfpl4a086&_nc_ohc=4FHVr7aI6sYAX-n0HLO&_nc_ht=scontent.fdac151-1.fna&oh=03_AdT75Hvg4NzRPcxDhqYD4xxuxvcPDBP9wr3zA80zkKOZ3A&oe=654771B8";
