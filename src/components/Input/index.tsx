65de8fba572f5cbb136bf4db723204acb93b4388
Ariful islam <arifulbgt4@gmail.com>
2023-10-11 19:39:48 +0600

    feat: Upload image input field

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,3 +9,4 @@ export { default as Switch } from "./Switch";
 export { default as Select } from "./Select";
 export { default as RadioGroup } from "./RadioGroup";
 export { default as Checkbox } from "./Checkbox";
+export { default as UploadImage } from "./UploadImage";
