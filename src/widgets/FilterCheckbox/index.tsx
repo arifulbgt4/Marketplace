c010000702c8450e00671000be4629406ddc9203
devcowsar <kawsarcse836@gmail.com>
2023-10-25 07:37:09 -0700

    feat: filtercheckbox

---
+++ src/widgets/FilterCheckbox/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import { FilterCheckboxProps } from "./Types";
+
+const FilterCheckbox: FC<FilterCheckboxProps> = () => {
+  return <div>FilterCheckbox</div>;
+};
+
+export default FilterCheckbox;
