aec86df09166bbb52a69c15a5a18525de7abee38
devcowsar <kawsarcse836@gmail.com>
2023-10-26 09:26:04 -0700

    fix: filtercheckbox

--- src/widgets/FilterCheckbox/index.tsx
+++ src/widgets/FilterCheckbox/index.tsx
@@ -1,21 +1,41 @@
 "use client";
 import { FC } from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import {
+  FormControl,
+  FormGroup,
+  FormControlLabel,
+  Grid,
+  Typography,
+} from "@mui/material";
 
 import { Checkbox } from "src/components/Input";
-import { Form as FinalForm } from "react-final-form";
-import FilterCheckboxForm from "../../forms/FilterCheckboxForm/index";
-import { checkbox } from "src/global/staticData/index";
+
 import { FilterCheckboxProps } from "./Types";
 
-const FilterCheckbox: FC<FilterCheckboxProps> = ({ title }) => {
+const FilterCheckbox: FC<FilterCheckboxProps> = ({ title, checkbox }) => {
   return (
     <Grid container rowGap={2}>
       <Grid item xs={12}>
         <Typography variant="h5">{title}</Typography>
       </Grid>
       <Grid xs={12}>
-        <FilterCheckboxForm checkbox={checkbox} />
+        <Grid container rowGap={2}>
+          {checkbox.map((data) => {
+            const { label, value } = data;
+            return (
+              <Grid item xs={6} key={value}>
+                <FormControl component="fieldset" variant="standard">
+                  <FormGroup>
+                    <FormControlLabel
+                      control={<Checkbox name={value} />}
+                      label={label}
+                    />
+                  </FormGroup>
+                </FormControl>
+              </Grid>
+            );
+          })}
+        </Grid>
       </Grid>
     </Grid>
   );
