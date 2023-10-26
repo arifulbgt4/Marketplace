7b795f06d12dd96a24ce36199293c466a9871f98
devcowsar <kawsarcse836@gmail.com>
2023-10-26 03:38:43 -0700

    fix : importfiltercheckbox

--- src/app/[locale]/s/page.tsx
+++ src/app/[locale]/s/page.tsx
@@ -2,7 +2,6 @@ import { Box, Container, Divider, Grid } from "@mui/material";
 
 import ListSearchFilters from "src/widgets/ListSearchFilters";
 import SearchListingGroup from "src/widgets/SearchListingGroup";
-import FilterCheckbox from "../../../widgets/FilterCheckbox/index";
 
 const SearchPage = () => {
   return (
@@ -11,9 +10,6 @@ const SearchPage = () => {
         <Grid container>
           <Grid item xs={3}>
             <ListSearchFilters />
-            <Divider />
-            <FilterCheckbox title="property type" />
-            <Divider />
           </Grid>
           <Grid item xs={9}>
             <SearchListingGroup />
