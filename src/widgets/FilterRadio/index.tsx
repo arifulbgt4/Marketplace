fd580e1ed0cdd394e567bd4cfd949eec807de1b9
md-cowsar-ahmed-257 <kawsarcse836@gmail.com>
2023-10-27 12:29:48 +0600

    feat: filterradio

---
+++ src/widgets/FilterRadio/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import { FilterRadioProps } from "./Types";
+
+const FilterRadio: FC<FilterRadioProps> = () => {
+  return <div>FilterRadio</div>;
+};
+
+export default FilterRadio;
