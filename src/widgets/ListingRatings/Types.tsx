bf628eac9b52a7660759cc2c21a45e083a87090b
jueal hossain <mdjueal920977@gmail.com>
2023-10-27 14:43:48 +0600

    feat: ListingRatings

---
+++ src/widgets/ListingRatings/Types.tsx
@@ -0,0 +1,12 @@
+export interface ListingRatingsProps {
+  rating: {
+    cleanliness: number;
+    communication: number;
+    checkIn: number;
+    accuracy: number;
+    location: number;
+    value: number;
+  };
+  avaragerating: number;
+  review: number;
+}
