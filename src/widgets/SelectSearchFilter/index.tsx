dc66cb02f7a1729a88c8836a95f32c93ffa2e684
devcowsar <kawsarcse836@gmail.com>
2023-10-28 06:20:41 -0700

    fix: selectsearchfilter

--- src/widgets/SelectSearchFilter/index.tsx
+++ src/widgets/SelectSearchFilter/index.tsx
@@ -17,27 +17,39 @@ const SelectSearchFilter: FC<SelectSearchFilterProps> = () => {
   return (
     <Grid container gap={2}>
       <Grid item xs={12}>
-        <Typography variant="h5">Bedrooms</Typography>
-      </Grid>
-      <Grid item xs={5}>
-        <Typography variant="subtitle1">Min beds</Typography>
+        <Typography variant="subtitle1">Radius</Typography>
         <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">No Min</InputLabel>
-          <Select value={selectitems} name="minbeds">
-            <MenuItem value={0}>min</MenuItem>
-            <MenuItem value={1}>nomin</MenuItem>
+          <InputLabel id="radius-label">There are only</InputLabel>
+          <Select id="radius-select" value={selectitems} name="radius">
+            <MenuItem value={0}>sample1</MenuItem>
+            <MenuItem value={1}>sample2</MenuItem>
           </Select>
         </FormControl>
       </Grid>
-      <Grid item xs={5}>
-        <Typography variant="subtitle1">Max beds</Typography>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">no Max</InputLabel>
-          <Select id="demo-simple-select" value={selectitems} name="maxbeds">
-            <MenuItem value={0}>max</MenuItem>
-            <MenuItem value={1}>nomax</MenuItem>
-          </Select>
-        </FormControl>
+      <Grid item xs={12} container gap={2}>
+        <Grid item xs={12}>
+          <Typography variant="h5">Bedrooms</Typography>
+        </Grid>
+        <Grid item xs={5}>
+          <Typography variant="subtitle1">Min beds</Typography>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">No Min</InputLabel>
+            <Select value={selectitems} name="minbeds">
+              <MenuItem value={0}>min</MenuItem>
+              <MenuItem value={1}>nomin</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={5}>
+          <Typography variant="subtitle1">Max beds</Typography>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">no Max</InputLabel>
+            <Select id="demo-simple-select" value={selectitems} name="maxbeds">
+              <MenuItem value={0}>max</MenuItem>
+              <MenuItem value={1}>nomax</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
       </Grid>
     </Grid>
   );
