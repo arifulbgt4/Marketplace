4e85821b9b7db93866095fcd0b3d401afaf15832
devcowsar <kawsarcse836@gmail.com>
2023-10-28 09:27:24 -0700

    feat: filterbuttongroup

--- src/widgets/SearchListingGroup/index.tsx
+++ src/widgets/SearchListingGroup/index.tsx
@@ -4,6 +4,8 @@ import { Grid, Box, Stack } from "@mui/material";
 import { searchListingData } from "src/global/staticData";
 import Listing from "../Listing";
 import ListingPagination from "../ListingPagination";
+import SearchFilterForm from "src/forms/SearchFilterForm";
+import FitlerButtonGroup from "src/widgets/FilterButtonGroup";
 
 import { SearchListingGroupProps } from "./Types";
 
@@ -11,6 +13,20 @@ const SearchListingGroup: FC<SearchListingGroupProps> = () => {
   return (
     <Box py={5}>
       <Grid container rowSpacing={5}>
+        <Grid
+          container
+          item
+          xs={12}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Grid item xs={6}>
+            <SearchFilterForm />
+          </Grid>
+          <Grid item xs={4}>
+            <FitlerButtonGroup />
+          </Grid>
+        </Grid>
         <Grid item container xs={12} columnSpacing={4} rowSpacing={4}>
           {searchListingData.map((data) => {
             const {
