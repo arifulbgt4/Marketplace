30650d700138ae7764d0bb011e4b88f41d9944c4
devcowsar <kawsarcse836@gmail.com>
2023-10-28 21:18:19 -0700

    fix: buttogroup

--- src/widgets/FilterButtonGroup/index.tsx
+++ src/widgets/FilterButtonGroup/index.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import { Button, ButtonGroup } from "@mui/material";
-import AlarmIcon from "@mui/icons-material/Alarm";
+import AutoAwesomeMosaicIcon from "@mui/icons-material/AutoAwesomeMosaic";
+import MenuIcon from "@mui/icons-material/Menu";
+import LanguageIcon from "@mui/icons-material/Language";
 
 import { FilterButtonGroupProps } from "./Types";
 
@@ -11,9 +13,9 @@ const FitlerButtonGroup: FC<FilterButtonGroupProps> = () => {
       fullWidth
       aria-label="outlined primary button group"
     >
-      <Button startIcon={<AlarmIcon />}>Grid</Button>
-      <Button startIcon={<AlarmIcon />}>List</Button>
-      <Button startIcon={<AlarmIcon />}>Map</Button>
+      <Button startIcon={<AutoAwesomeMosaicIcon />}>Grid</Button>
+      <Button startIcon={<MenuIcon />}>List</Button>
+      <Button startIcon={<LanguageIcon />}>Map</Button>
     </ButtonGroup>
   );
 };
