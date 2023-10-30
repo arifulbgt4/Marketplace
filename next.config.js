b580f545d1cbd8036e5dccbabc873ca5cbb712df
Ariful islam <arifulbgt4@gmail.com>
2023-10-30 10:25:22 +0600

    chore: uupdate Next 14

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images:{
-    domains:["react-photo-album.com","source.unsplash.com"]
-  },
-  experimental: {
-    serverActions: true,
+  images: {
+    domains: ["react-photo-album.com", "source.unsplash.com"],
   },
   compiler: {
     styledComponents: true,
