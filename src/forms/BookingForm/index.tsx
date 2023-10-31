ed9a21cdb38373d21e5e81e4437457476c6e338d
devcowsar <kawsarcse836@gmail.com>
2023-10-31 07:45:39 -0700

    fix: bookingform

--- src/forms/BookingForm/index.tsx
+++ src/forms/BookingForm/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { FC, useState } from "react";
+import { Form as FinalForm } from "react-final-form";
 import {
   Paper,
   Stack,
@@ -7,91 +8,104 @@ import {
   Divider,
   Button,
   Rating,
-  Select,
   InputLabel,
   Box,
+  MenuItem,
+  FormControl,
 } from "@mui/material";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
 import { LocalizationProvider } from "@mui/x-date-pickers-pro";
 import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
 import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
 
+import { Select } from "src/components/Input";
+
 import { BookingFormProps } from "./Types";
 
 const BookingForm: FC<BookingFormProps> = () => {
-  const [gest, setGest] = useState("");
+  const gest = "";
 
-  const handleChange = (event: any) => {
-    setGest(event.target.value as string);
-  };
-
+  const onSubmitForm = async () => {};
   return (
     <Paper>
-      <Stack py={5} px={3} gap={3}>
-        <Stack
-          flexDirection="row"
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          <Typography variant="h5">$500/month</Typography>
-          <Stack flexDirection="row">
-            <Rating defaultValue={1} max={1} />
-            <Typography>12 reviews</Typography>
-          </Stack>
-        </Stack>
-        <Stack>
-          <Stack flexDirection="row" justifyContent="space-between" pr={5.8}>
-            <Typography>Check-in</Typography>
-            <Typography>Checkout</Typography>
-          </Stack>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateRangePicker
-              defaultRangePosition="end"
-              localeText={{ start: "", end: "" }}
-            />
-          </LocalizationProvider>
-        </Stack>
-        <Box>
-          <InputLabel>Gestes</InputLabel>
-          <FormControl fullWidth>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={gest}
-              onChange={handleChange}
-            >
-              <MenuItem value={1}>1</MenuItem>
-              <MenuItem value={2}>2</MenuItem>
-              <MenuItem value={3}>3</MenuItem>
-              <MenuItem value={4}>4</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-        <Button variant="contained" size="large">
-          reserve
-        </Button>
-        <Typography textAlign="center">You won't be charged yet</Typography>
-        <Stack gap={2}>
-          <Stack flexDirection="row" justifyContent="space-between">
-            <Typography>$500 x 5 month</Typography>
-            <Typography>$2500</Typography>
-          </Stack>
-          <Stack flexDirection="row" justifyContent="space-between">
-            <Typography>Long stay discount</Typography>
-            <Typography>-$100</Typography>
-          </Stack>
-          <Stack flexDirection="row" justifyContent="space-between">
-            <Typography>Airbnb service fee</Typography>
-            <Typography>$150</Typography>
-          </Stack>
-        </Stack>
-        <Divider></Divider>
-        <Stack flexDirection="row" justifyContent="space-between">
-          <Typography variant="h6">Total before taxes</Typography>
-          <Typography>$2550</Typography>
-        </Stack>
-      </Stack>
+      <FinalForm
+        onSubmit={onSubmitForm}
+        render={({ handleSubmit, values, errors, submitting }) => {
+          return (
+            <form onSubmit={handleSubmit}>
+              <Stack py={5} px={3} gap={3}>
+                <Stack
+                  flexDirection="row"
+                  alignItems="center"
+                  justifyContent="space-between"
+                >
+                  <Typography variant="h5">$500/month</Typography>
+                  <Stack flexDirection="row">
+                    <Rating defaultValue={1} max={1} />
+                    <Typography>12 reviews</Typography>
+                  </Stack>
+                </Stack>
+                <Stack>
+                  <Stack
+                    flexDirection="row"
+                    justifyContent="space-between"
+                    pr={5.8}
+                  >
+                    <Typography>Check-in</Typography>
+                    <Typography>Checkout</Typography>
+                  </Stack>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateRangePicker
+                      defaultRangePosition="end"
+                      localeText={{ start: "", end: "" }}
+                    />
+                  </LocalizationProvider>
+                </Stack>
+                <Box>
+                  <InputLabel>Gestes</InputLabel>
+                  <FormControl fullWidth>
+                    <Select
+                      labelId="gestes-select-label"
+                      id="gestes-select"
+                      value={gest}
+                      name="gestes"
+                    >
+                      <MenuItem value={1}>1</MenuItem>
+                      <MenuItem value={2}>2</MenuItem>
+                      <MenuItem value={3}>3</MenuItem>
+                      <MenuItem value={4}>4</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+                <Button variant="contained" size="large">
+                  reserve
+                </Button>
+                <Typography textAlign="center">
+                  You won't be charged yet
+                </Typography>
+                <Stack gap={2}>
+                  <Stack flexDirection="row" justifyContent="space-between">
+                    <Typography>$500 x 5 month</Typography>
+                    <Typography>$2500</Typography>
+                  </Stack>
+                  <Stack flexDirection="row" justifyContent="space-between">
+                    <Typography>Long stay discount</Typography>
+                    <Typography>-$100</Typography>
+                  </Stack>
+                  <Stack flexDirection="row" justifyContent="space-between">
+                    <Typography>Airbnb service fee</Typography>
+                    <Typography>$150</Typography>
+                  </Stack>
+                </Stack>
+                <Divider></Divider>
+                <Stack flexDirection="row" justifyContent="space-between">
+                  <Typography variant="h6">Total before taxes</Typography>
+                  <Typography>$2550</Typography>
+                </Stack>
+              </Stack>
+            </form>
+          );
+        }}
+      />
     </Paper>
   );
 };
