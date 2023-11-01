cd8e1c99fe0a39f549ccc12c5e7b4442da715b4c
jueal hossain <mdjueal920977@gmail.com>
2023-11-02 01:01:53 +0600

    fix: SearchFilterForm

--- src/forms/SearchFilterForm/index.tsx
+++ src/forms/SearchFilterForm/index.tsx
@@ -1,13 +1,11 @@
-"use client";
-import { FC, useState } from "react";
+import { FC } from "react";
+import { Form as FinalForm } from "react-final-form";
 import {
   Paper,
-  Box,
   FormControl,
   Hidden,
   Button,
   Grid,
-  Drawer,
   InputLabel,
   InputAdornment,
   OutlinedInput,
@@ -15,55 +13,21 @@ import {
 import SearchSharpIcon from "@mui/icons-material/SearchSharp";
 import LocationOnSharpIcon from "@mui/icons-material/LocationOnSharp";
 import IconButton from "@mui/material/IconButton";
-import FilterAltIcon from "@mui/icons-material/FilterAlt";
-
-import { Form as FinalForm } from "react-final-form";
 
 import { SearchFilterFormProps } from "./Types";
-import ListSearchFilters from "src/widgets/ListSearchFilters";
 
 const SearchFilterForm: FC<SearchFilterFormProps> = ({}) => {
-  const [open, setOpen] = useState(false);
   const onSubmitForm = async () => {};
 
   return (
-    <Paper sx={{ pb: { md: 1 } }}>
+    <Paper>
       <FinalForm
         onSubmit={onSubmitForm}
         render={({ handleSubmit, values, errors, submitting }) => {
           return (
             <form onSubmit={handleSubmit}>
               <Grid container>
-                <Hidden mdUp>
-                  <Grid
-                    item
-                    xs={1}
-                    md={0}
-                    sx={{ display: "flex", justifyContent: "center" }}
-                  >
-                    <IconButton
-                      onClick={() => setOpen(true)}
-                      sx={(theme) => ({
-                        border: `1px solid ${theme.palette.grey[700]}`,
-                        borderRadius: 0.5,
-                        width: "100%",
-                        p: 0,
-                        py: 1.4,
-                      })}
-                    >
-                      <FilterAltIcon sx={{ height: 30, width: 30 }} />
-                    </IconButton>
-                  </Grid>
-                  <Drawer open={open} onClose={() => setOpen(false)}>
-                    <Box width={{ xs: 250, sm: 300 }}>
-                      <ListSearchFilters
-                        onCloseMobileDrawer={() => setOpen(false)}
-                      />
-                    </Box>
-                  </Drawer>
-                </Hidden>
-
-                <Grid item xs={5}>
+                <Grid item xs={5.5} md={5}>
                   <FormControl variant="outlined" fullWidth>
                     <InputLabel htmlFor="outlined-adornment-location">
                       Location
@@ -80,7 +44,7 @@ const SearchFilterForm: FC<SearchFilterFormProps> = ({}) => {
                     />
                   </FormControl>
                 </Grid>
-                <Grid item xs={5}>
+                <Grid item xs={5.5} md={5}>
                   <FormControl variant="outlined" fullWidth>
                     <InputLabel htmlFor="outlined-adornment-listing">
                       Key
@@ -103,6 +67,7 @@ const SearchFilterForm: FC<SearchFilterFormProps> = ({}) => {
                       Search
                     </Button>
                   </Hidden>
+
                   <Hidden mdUp>
                     <IconButton
                       sx={(theme) => ({
--- src/widgets/SearchListingGroup/index.tsx
+++ src/widgets/SearchListingGroup/index.tsx
@@ -1,16 +1,39 @@
 "use client";
 import { FC, useState } from "react";
-import { Grid, Box, Stack, Hidden } from "@mui/material";
+import {
+  Grid,
+  Box,
+  Stack,
+  Hidden,
+  IconButton,
+  Drawer,
+  Typography,
+  Paper,
+  Modal,
+  ButtonBase,
+  Button,
+  InputAdornment,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import SearchSharpIcon from "@mui/icons-material/SearchSharp";
+import LocationOnSharpIcon from "@mui/icons-material/LocationOnSharp";
+import CloseIcon from "@mui/icons-material/Close";
 
 import { searchListingData } from "src/global/staticData";
 import SearchFilterForm from "src/forms/SearchFilterForm";
 import ListGridView from "src/widgets/ListGridView";
 import Listing from "../Listing";
 import ListingPagination from "../ListingPagination";
+import ListSearchFilters from "../ListSearchFilters";
 
 import { SearchListingGroupProps } from "./Types";
 
 const SearchListingGroup: FC<SearchListingGroupProps> = () => {
+  const [open, setOpen] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
   const [isGrid, setIsGrid] = useState(true);
 
   const toggleGridList = () => {
@@ -24,17 +47,136 @@ const SearchListingGroup: FC<SearchListingGroupProps> = () => {
           container
           item
           xs={12}
-          justifyContent="space-between"
+          justifyContent={{ xs: "space-between" }}
           alignItems="center"
         >
-          <Grid item xs={12} lg={6}>
-            <SearchFilterForm />
-          </Grid>
-          <Grid item md={12} lg={3}>
-            <Hidden mdDown>
+          <Hidden mdUp>
+            <Grid item xs={12} display="flex">
+              <IconButton
+                onClick={() => setOpen(true)}
+                sx={{
+                  border: "1px solid #fff",
+                  px: 2.2,
+                }}
+              >
+                <FilterAltIcon />
+              </IconButton>
+
+              <Drawer open={open} onClose={() => setOpen(false)}>
+                <Box width={{ xs: 250, sm: 300 }}>
+                  <ListSearchFilters
+                    onCloseMobileDrawer={() => setOpen(false)}
+                  />
+                </Box>
+              </Drawer>
+              <Paper sx={{ width: "100%" }}>
+                <Stack
+                  component={ButtonBase}
+                  width="100%"
+                  flexDirection="row"
+                  justifyContent="flex-start"
+                  border="1px solid #fff"
+                  borderRadius={12.5}
+                  py={0.5}
+                  ml={{ xs: 1.3 }}
+                  onClick={() => {
+                    setOpenModal(true);
+                  }}
+                >
+                  <IconButton sx={{ px: 2.5 }}>
+                    <SearchSharpIcon />
+                  </IconButton>
+                  <Stack>
+                    <Typography>Search section</Typography>
+                    <Typography
+                      color="text.secondary"
+                      variant="subtitle2"
+                      textAlign="start"
+                    >
+                      Location
+                    </Typography>
+                  </Stack>
+                </Stack>
+                <Modal
+                  open={openModal}
+                  onClose={() => {
+                    setOpenModal(false);
+                  }}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                  <Stack
+                    gap={1}
+                    sx={{
+                      position: "absolute" as "absolute",
+                      top: "50%",
+                      left: "50%",
+                      transform: "translate(-50%, -50%)",
+                      width: "100vw",
+                      height: "100%",
+                      bgcolor: "background.paper",
+
+                      boxShadow: 24,
+                      p: 2,
+                    }}
+                  >
+                    <IconButton
+                      onClick={() => {
+                        setOpenModal(false);
+                      }}
+                      sx={{ justifyContent: "flex-start" }}
+                    >
+                      <CloseIcon />
+                    </IconButton>
+                    <Typography variant="h5">Where to ?</Typography>
+                    <FormControl variant="outlined" fullWidth>
+                      <InputLabel htmlFor="outlined-adornment-location">
+                        Location
+                      </InputLabel>
+                      <OutlinedInput
+                        id="outlined-adornment-location"
+                        endAdornment={
+                          <InputAdornment position="end">
+                            <IconButton>
+                              <LocationOnSharpIcon />
+                            </IconButton>
+                          </InputAdornment>
+                        }
+                      />
+                    </FormControl>
+                    <FormControl variant="outlined" fullWidth>
+                      <InputLabel htmlFor="outlined-adornment-listing">
+                        Key
+                      </InputLabel>
+                      <OutlinedInput
+                        id="outlined-adornment-password"
+                        endAdornment={
+                          <InputAdornment position="start">
+                            <IconButton>
+                              <SearchSharpIcon />
+                            </IconButton>
+                          </InputAdornment>
+                        }
+                      />
+                    </FormControl>
+                    <Button fullWidth variant="contained" sx={{ py: 2 }}>
+                      Search
+                    </Button>
+                  </Stack>
+                </Modal>
+              </Paper>
+            </Grid>
+          </Hidden>
+          <Hidden mdDown>
+            <Grid item md={6}>
+              <SearchFilterForm />
+            </Grid>
+          </Hidden>
+          <Hidden mdDown>
+            <Grid md={3}>
               <ListGridView isGrid={isGrid} toggleGridList={toggleGridList} />
-            </Hidden>
-          </Grid>
+            </Grid>
+          </Hidden>
         </Grid>
         <Grid item container xs={12} columnSpacing={4} rowSpacing={4}>
           {searchListingData.map((data) => {
