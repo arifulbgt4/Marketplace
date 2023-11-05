5172b8e8694991f7289af218a64d8c533a285bc2
devcowsar <kawsarcse836@gmail.com>
2023-11-05 00:14:52 -0700

    feat: listingformverticalstepper

---
+++ src/widgets/VerticalStepper/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import { VerticalStepperProps } from "./Types";
+
+const VerticalStepper: FC<VerticalStepperProps> = () => {
+  return <div>VerticalSteppe</div>;
+};
+
+export default VerticalStepper;
