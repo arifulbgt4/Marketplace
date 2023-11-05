069b280f82c77e9067078bcd4b3eb14111d06650
devcowsar <kawsarcse836@gmail.com>
2023-11-05 06:54:15 -0800

    fix: stepper

--- src/widgets/VerticalStepper/index.tsx
+++ src/widgets/VerticalStepper/index.tsx
@@ -1,9 +1,167 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import {
+  Box,
+  Stepper,
+  Step,
+  StepLabel,
+  StepContent,
+  Button,
+  Paper,
+  Typography,
+} from "@mui/material";
 
 import { VerticalStepperProps } from "./Types";
+import { TextField } from "src/components/Input";
 
 const VerticalStepper: FC<VerticalStepperProps> = () => {
-  return <div>VerticalSteppe</div>;
+  const [activeStep, setActiveStep] = useState(0);
+  const [isActiveStep, setIsActiveStep] = useState(false);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 400 }}>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        <Step>
+          <StepLabel>
+            <Typography>Basic Information</Typography>
+            {activeStep == 0 ? (
+              <Typography variant="caption">pending</Typography>
+            ) : (
+              <Typography>complete</Typography>
+            )}
+          </StepLabel>
+
+          <StepContent>
+            <Box sx={{ mb: 2 }}>
+              <div>
+                <Button
+                  variant="contained"
+                  onClick={handleNext}
+                  sx={{ mt: 1, mr: 1 }}
+                >
+                  Continue
+                </Button>
+                <Button
+                  onClick={handleBack}
+                  sx={{ mt: 1, mr: 1 }}
+                  disabled={true}
+                >
+                  Back
+                </Button>
+              </div>
+            </Box>
+          </StepContent>
+        </Step>
+        <Step>
+          <StepLabel>
+            <Typography>Basic Information</Typography>
+            {activeStep === 1 ? (
+              <Typography variant="caption">pending</Typography>
+            ) : (
+              <Typography>complete</Typography>
+            )}
+          </StepLabel>
+          <StepContent>
+            <Typography>description</Typography>
+            <Box sx={{ mb: 2 }}>
+              <div>
+                <Button
+                  variant="contained"
+                  onClick={handleNext}
+                  sx={{ mt: 1, mr: 1 }}
+                >
+                  Continue
+                </Button>
+                <Button onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+                  Back
+                </Button>
+              </div>
+            </Box>
+          </StepContent>
+        </Step>
+        <Step>
+          <StepLabel>Basic Information</StepLabel>
+          <StepContent>
+            <Typography>description</Typography>
+            <Box sx={{ mb: 2 }}>
+              <div>
+                <Button
+                  variant="contained"
+                  onClick={handleNext}
+                  sx={{ mt: 1, mr: 1 }}
+                >
+                  Continue
+                </Button>
+                <Button onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+                  Back
+                </Button>
+              </div>
+            </Box>
+          </StepContent>
+        </Step>
+        <Step>
+          <StepLabel>Basic Information</StepLabel>
+          <StepContent>
+            <Typography>description</Typography>
+            <Box sx={{ mb: 2 }}>
+              <div>
+                <Button
+                  variant="contained"
+                  onClick={handleNext}
+                  sx={{ mt: 1, mr: 1 }}
+                >
+                  Continue
+                </Button>
+                <Button onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+                  Back
+                </Button>
+              </div>
+            </Box>
+          </StepContent>
+        </Step>
+        <Step>
+          <StepLabel>Basic Information</StepLabel>
+          <StepContent>
+            <Typography>description</Typography>
+            <Box sx={{ mb: 2 }}>
+              <div>
+                <Button
+                  variant="contained"
+                  onClick={handleNext}
+                  sx={{ mt: 1, mr: 1 }}
+                >
+                  Continue
+                </Button>
+                <Button onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+                  Back
+                </Button>
+              </div>
+            </Box>
+          </StepContent>
+        </Step>
+      </Stepper>
+
+      {activeStep === 5 && (
+        <Paper square elevation={0} sx={{ p: 3 }}>
+          <Typography>All steps completed - you&apos;re finished</Typography>
+          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+            Reset
+          </Button>
+        </Paper>
+      )}
+    </Box>
+  );
 };
 
 export default VerticalStepper;
