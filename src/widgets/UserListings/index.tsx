f74d8e9be3b76d03ceb0663e2ceea9ba27c68557
jueal hossain <mdjueal920977@gmail.com>
2023-11-07 17:40:38 +0600

    fix: host profile

--- src/widgets/UserListings/index.tsx
+++ src/widgets/UserListings/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Grid } from "@mui/material";
+import { Grid, Typography, Divider } from "@mui/material";
 
 import { userListingData } from "src/global/staticData";
 import Listing from "../Listing";
@@ -8,25 +8,39 @@ import { UserListingsProps } from "./Types";
 
 const UserListings: FC<UserListingsProps> = () => {
   return (
-    <Grid container spacing={5}>
-      {userListingData.map((data) => {
-        const { id, image, title, price, description, rating, slug, address } =
-          data;
-        return (
-          <Grid item xs={12} md={6} key={id}>
-            <Listing
-              id={id}
-              slug={slug}
-              image={image}
-              title={title}
-              price={price}
-              description={description}
-              rating={rating}
-              address={address}
-            />
-          </Grid>
-        );
-      })}
+    <Grid container rowSpacing={2}>
+      <Grid item xs={12} display="flex" gap={1}>
+        <Typography variant="h5">Listings</Typography>
+        <Divider />
+      </Grid>
+      <Grid item container xs={12} spacing={5}>
+        {userListingData.map((data) => {
+          const {
+            id,
+            image,
+            title,
+            price,
+            description,
+            rating,
+            slug,
+            address,
+          } = data;
+          return (
+            <Grid item xs={12} md={6} key={id}>
+              <Listing
+                id={id}
+                slug={slug}
+                image={image}
+                title={title}
+                price={price}
+                description={description}
+                rating={rating}
+                address={address}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
     </Grid>
   );
 };
