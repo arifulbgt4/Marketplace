d9ac02706b5c4d2c31bb343e694b1c4a5811b229
jueal hossain <mdjueal920977@gmail.com>
2023-11-07 17:40:38 +0600

    fix: host profile

--- src/widgets/UserReviews/index.tsx
+++ src/widgets/UserReviews/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Grid, Button } from "@mui/material";
+import { Grid, Button, Typography, Divider } from "@mui/material";
 
 import { userReviewData } from "src/global/staticData";
 import Review from "../Review";
@@ -8,25 +8,31 @@ import { UserReviewsProps } from "./Types";
 
 const UserReviews: FC<UserReviewsProps> = () => {
   return (
-    <Grid container spacing={4}>
-      {userReviewData.map((data) => {
-        const { id, title, subheader, description, img } = data;
-        return (
-          <Grid key={id} item xs={12}>
-            <Review
-              id={id}
-              title={title}
-              subheader={subheader}
-              description={description}
-              img={img}
-            />
-          </Grid>
-        );
-      })}
-      <Grid item xs={12}>
-        <Button variant="outlined" color="inherit" size="large">
-          Show all 24 reviews
-        </Button>
+    <Grid container rowSpacing={2}>
+      <Grid item xs={12} display="flex" gap={1}>
+        <Typography variant="h5">Listings</Typography>
+        <Divider />
+      </Grid>
+      <Grid item container xs={12} spacing={4}>
+        {userReviewData.map((data) => {
+          const { id, title, subheader, description, img } = data;
+          return (
+            <Grid key={id} item xs={12}>
+              <Review
+                id={id}
+                title={title}
+                subheader={subheader}
+                description={description}
+                img={img}
+              />
+            </Grid>
+          );
+        })}
+        <Grid item xs={12}>
+          <Button variant="outlined" color="inherit" size="large">
+            Show all 24 reviews
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
   );
