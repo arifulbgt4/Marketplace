b98f8eef85aa9e32ce307af5b6c8338cf38d6442
jueal hossain <mdjueal920977@gmail.com>
2023-11-12 22:26:39 +0600

    fix:  dashboard mobile view

--- src/app/[locale]/u/(User)/dashboard/page.tsx
+++ src/app/[locale]/u/(User)/dashboard/page.tsx
@@ -6,21 +6,17 @@ import UserMap from "src/widgets/UserMap";
 
 const DashboardPage = () => {
   return (
-    <Box>
-      <Container>
-        <Grid container rowSpacing={5} columnSpacing={10}>
-          <Grid item xs={12} md={8}>
-            <Stack gap={6}>
-              <StatisticsGroup />
-              <UserMap />
-            </Stack>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <DashboardHelpInfo />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
+    <Grid container rowSpacing={5} columnSpacing={8}>
+      <Grid item xs={12} md={8}>
+        <Stack gap={6}>
+          <StatisticsGroup />
+          <UserMap />
+        </Stack>
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <DashboardHelpInfo />
+      </Grid>
+    </Grid>
   );
 };
 
