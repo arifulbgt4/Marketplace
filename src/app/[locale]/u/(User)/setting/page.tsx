5bb2b16ee262a3ab217587e2e0cc563b20be312d
jueal hossain <mdjueal920977@gmail.com>
2023-11-12 17:12:59 +0600

    fix: setting

--- src/app/[locale]/u/(User)/setting/page.tsx
+++ src/app/[locale]/u/(User)/setting/page.tsx
@@ -1,7 +1,12 @@
+import { Box, Container } from "@mui/material";
 import UserSettingForm from "src/forms/UserSettingForm";
 
 const Settings = () => {
-  return <UserSettingForm />;
+  return (
+    <Box p={5}>
+      <UserSettingForm />
+    </Box>
+  );
 };
 
 export default Settings;
