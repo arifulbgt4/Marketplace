bafa469180bca8766b8d93aff2dcbf3fc14c6339
Ariful islam <arifulbgt4@gmail.com>
2023-11-15 10:10:23 +0600

    fix: search banner BG image

--- src/widgets/SearchBanner/index.tsx
+++ src/widgets/SearchBanner/index.tsx
@@ -5,16 +5,16 @@ import SearchFilterForm from "src/forms/SearchFilterForm";
 
 import { SearchBannerProps } from "./Types";
 
-const bannaerimg =
-  "https://scontent.fdac151-1.fna.fbcdn.net/v/t1.15752-9/384820780_347011921016699_8313359896565317700_n.png?_nc_cat=108&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeEsLQgGhu08CR-DeUfDtMCe5CjaR2Hn_ezkKNpHYef97N935-sm0XaD9SVIEYXyWpTvjQrW4un2e_UAfpl4a086&_nc_ohc=4FHVr7aI6sYAX-n0HLO&_nc_ht=scontent.fdac151-1.fna&oh=03_AdT75Hvg4NzRPcxDhqYD4xxuxvcPDBP9wr3zA80zkKOZ3A&oe=654771B8";
+const BANNER_BG =
+  "https://images.unsplash.com/photo-1500964757637-c85e8a162699?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=925&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTcwMDAyMTMzMg&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1920";
 
 const SearchBanner: FC<SearchBannerProps> = () => {
   return (
     <Stack
       component="div"
       sx={{
-        minHeight: 600,
-        backgroundImage: `url(${bannaerimg})`,
+        minHeight: "100vh",
+        backgroundImage: `url(${BANNER_BG})`,
         backgroundPosition: "center",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
