2cb435eae486d55a2bea0579e9439f96225ab616
Ariful islam <arifulbgt4@gmail.com>
2023-11-15 10:10:23 +0600

    fix: search banner BG image

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "react-photo-album.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
   compiler: {
     styledComponents: true,
   },
