9bedaf871ccc8930aa18ce044534a960b42809ba
devcowsar <kawsarcse836@gmail.com>
2023-11-15 04:34:04 -0800

    fix : mobilestepperform

--- src/forms/ListingForm/Staper.tsx
+++ src/forms/ListingForm/Staper.tsx
@@ -43,34 +43,43 @@ const Staper: FC<StaperProps> = () => {
       )}
 
       <Box maxWidth={400} marginX="auto" pt={3}>
-        <MobileStepper
-          variant="progress"
-          steps={5}
-          position="static"
-          activeStep={activeStep}
-          nextButton={
-            <Button
-              size="small"
-              onClick={handleNext}
-              disabled={activeStep === 4}
-            >
-              Next
-              {theme.direction === "rtl" ? (
-                <KeyboardArrowLeft />
-              ) : (
-                <KeyboardArrowRight />
-              )}
-            </Button>
-          }
-          backButton={
-            <Button
-              size="small"
-              onClick={handleBack}
-              disabled={activeStep === 0}
-            >
-              Back
-            </Button>
-          }
+        <FinalForm
+          onSubmit={onSubmitForm}
+          render={({ handleSubmit, values, errors, submitting }) => {
+            return (
+              <form onSubmit={handleSubmit}>
+                <MobileStepper
+                  variant="progress"
+                  steps={5}
+                  position="static"
+                  activeStep={activeStep}
+                  nextButton={
+                    <Button
+                      size="small"
+                      onClick={handleNext}
+                      disabled={activeStep === 4}
+                    >
+                      Next
+                      {theme.direction === "rtl" ? (
+                        <KeyboardArrowLeft />
+                      ) : (
+                        <KeyboardArrowRight />
+                      )}
+                    </Button>
+                  }
+                  backButton={
+                    <Button
+                      size="small"
+                      onClick={handleBack}
+                      disabled={activeStep === 0}
+                    >
+                      Back
+                    </Button>
+                  }
+                />
+              </form>
+            );
+          }}
         />
       </Box>
     </Box>
