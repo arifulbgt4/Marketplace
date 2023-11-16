cdaabebbf742b8436669d5288b4bfc0c9cbd2a1b
devcowsar <kawsarcse836@gmail.com>
2023-11-15 23:14:49 -0800

    feat : messagePage

---
+++ src/app/[locale]/message/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+const messagePage = () => {
+  return <Typography>ChatApplication</Typography>;
+};
+
+export default messagePage;
