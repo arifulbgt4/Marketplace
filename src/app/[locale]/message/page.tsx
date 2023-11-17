b33790c7993902993b3d03490cbc4ae9f72c2f86
devcowsar <kawsarcse836@gmail.com>
2023-11-17 06:38:52 -0800

    fix: messagepage

--- src/app/[locale]/message/page.tsx
+++ src/app/[locale]/message/page.tsx
@@ -1,12 +1,42 @@
 import { Container } from "@mui/material";
-import ChatApplication from "src/widgets/ChatApplication";
+import SearchPeopleForm from "src/forms/SearchPeopleForm";
+import Grid from "@mui/material/Grid";
+import ChatList from "src/widgets/ChatList";
+import ChatBox from "src/widgets/ChatBox";
+import SendMessageForm from "../../../forms/SendMessageForm/index";
 
-const messagePage = () => {
+const MessagePage = () => {
   return (
     <Container>
-      <ChatApplication />
+      <Grid container>
+        <Grid item xs={3}>
+          <Grid
+            item
+            xs={12}
+            px={2.8}
+            py={1.2}
+            borderBottom={
+              "1px solid var(--action-disabled-background, rgba(0, 0, 0, 0.12))"
+            }
+            borderRadius={1}
+          >
+            <SearchPeopleForm />
+          </Grid>
+          <Grid item xs={12}>
+            <ChatList />
+          </Grid>
+        </Grid>
+        <Grid item xs={9}>
+          <Grid item xs={12}>
+            <ChatBox />
+          </Grid>
+          <Grid item xs={12}>
+            <SendMessageForm />
+          </Grid>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
 
-export default messagePage;
+export default MessagePage;
---
+++ src/widgets/ChatBox/Types.ts
@@ -0,0 +1,1 @@
+export interface ChatBoxProps {}
