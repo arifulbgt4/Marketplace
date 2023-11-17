926000e3c99ce99c3a417309a01d1716b0bfef2b
jueal hossain <mdjueal920977@gmail.com>
2023-11-17 22:24:38 +0600

    fix: VisitorStatistic

--- src/app/[locale]/(WrappedPages)/u/(User)/dashboard/page.tsx
+++ src/app/[locale]/(WrappedPages)/u/(User)/dashboard/page.tsx
@@ -2,11 +2,38 @@ import { Grid } from "@mui/material";
 
 import VisitorStatistic from "src/widgets/VisitorStatistic";
 
+const uData = [
+  0, 900, 700, 1580, 1190, 1790, 700, 2750, 700, 2900, 700, 1890, 3290, 2990,
+];
+
+const visitorData = {
+  xLabels: [
+    "Page A",
+    "Page B",
+    "Page C",
+    "Page D",
+    "Page E",
+    "Page F",
+    "Page G",
+    "Page H",
+    "Page I",
+    "Page J",
+    "Page K",
+    "Page L",
+    "Page M",
+    "Page N",
+  ],
+  uData: [
+    0, 900, 700, 1580, 1190, 1790, 700, 2750, 700, 2900, 700, 1890, 3290, 2990,
+  ],
+  visitors: 345678,
+};
+
 const DashboardPage = () => {
   return (
     <Grid container rowSpacing={5} columnSpacing={8}>
       <Grid item xs={4}>
-        <VisitorStatistic />
+        <VisitorStatistic visitorData={visitorData} />
       </Grid>
     </Grid>
   );
