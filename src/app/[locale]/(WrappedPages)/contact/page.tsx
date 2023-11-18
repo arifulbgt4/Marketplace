d75b5529357196e53c9b4c813cf0422913a25ba2
jueal hossain <mdjueal920977@gmail.com>
2023-11-18 22:59:45 +0600

    fix: ListingHeader

--- src/app/[locale]/(WrappedPages)/contact/page.tsx
+++ src/app/[locale]/(WrappedPages)/contact/page.tsx
@@ -1,8 +1,8 @@
-import { Box, Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Paper } from "@mui/material";
 
 import BreadcumbBanner from "src/widgets/BreadcumbBanner";
-import ContactFormContainer from "src/widgets/Contact";
 import ConatactHelpInfo from "src/widgets/ConatactHelpInfo";
+import ContactForm from "src/forms/ContactForm";
 
 const Conatact = () => {
   return (
@@ -16,7 +16,9 @@ const Conatact = () => {
         <Container maxWidth="lg">
           <Grid container columnSpacing={8} rowSpacing={5}>
             <Grid item xs={12} md={8}>
-              <ContactFormContainer />
+              <Paper sx={{ p: { xs: 2, md: 4 } }}>
+                <ContactForm />
+              </Paper>
             </Grid>
             <Grid item xs={12} md={4}>
               <ConatactHelpInfo />
