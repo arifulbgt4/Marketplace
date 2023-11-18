c27563d3415d40e3f3f02d863e92a3ee3598bf5a
jueal hossain <mdjueal920977@gmail.com>
2023-11-18 23:56:40 +0600

    fix: remove listing map

--- src/app/[locale]/(WrappedPages)/u/(User)/listing/page.tsx
+++ src/app/[locale]/(WrappedPages)/u/(User)/listing/page.tsx
@@ -1,7 +1,5 @@
 import { Grid } from "@mui/material";
 
-import OwnListingMap from "src/widgets/OwnListingMap";
-
 import OwnListing from "src/widgets/OwnListing";
 
 const ListingPage = () => {
@@ -9,9 +7,6 @@ const ListingPage = () => {
     <Grid container columnSpacing={8} rowSpacing={5}>
       <Grid item xs={12} md={8}>
         <OwnListing />
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <OwnListingMap />
       </Grid>
     </Grid>
   );
