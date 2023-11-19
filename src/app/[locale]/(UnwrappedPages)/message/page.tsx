88c5345dc3be42f4cbbba58d196b104ceb731e94
devcowsar <kawsarcse836@gmail.com>
2023-11-19 02:36:45 -0800

    fix: messagepage

--- src/app/[locale]/(UnwrappedPages)/message/page.tsx
+++ src/app/[locale]/(UnwrappedPages)/message/page.tsx
@@ -1,41 +1,30 @@
 "use client";
-import { Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Stack } from "@mui/material";
 
-import SearchChatForm from "src/forms/SearchChatForm";
-import ChatList from "src/widgets/ChatList";
 import ChatBox from "src/widgets/ChatBox";
 import SendMessageForm from "src/forms/SendMessageForm";
+import MessageUsersSearch from "src/widgets/MessageUsersSearch";
 
 const MessagePage = () => {
   return (
-    <Container>
-      <Grid container>
-        <Grid item xs={3}>
-          <Grid
-            item
-            xs={12}
-            px={2.8}
-            py={1.2}
-            borderBottom={1}
-            borderColor={(theme) => theme.palette.action.disabledBackground}
-            borderRadius={1}
-          >
-            <SearchChatForm />
+    <Stack height="calc(100vh - 64px)">
+      <Container>
+        <Grid container>
+          <Grid item xs={3}>
+            <MessageUsersSearch />
           </Grid>
-          <Grid item xs={12}>
-            <ChatList />
+
+          <Grid item xs={9}>
+            <Grid item xs={12}>
+              <ChatBox />
+            </Grid>
+            <Grid item xs={12}>
+              <SendMessageForm />
+            </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={9}>
-          <Grid item xs={12}>
-            <ChatBox />
-          </Grid>
-          <Grid item xs={12}>
-            <SendMessageForm />
-          </Grid>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </Stack>
   );
 };
 
--- src/widgets/MessageUsersSearch/index.tsx
+++ src/widgets/MessageUsersSearch/index.tsx
@@ -1,8 +1,28 @@
 import { FC } from "react";
+import { Box, Grid, Stack } from "@mui/material";
+
+import SearchChatForm from "src/forms/SearchChatForm";
+import ChatList from "src/widgets/ChatList";
+
 import { MessageUsersSearchProps } from "./Types";
 
 const MessageUsersSearch: FC<MessageUsersSearchProps> = () => {
-  return <div>index</div>;
+  return (
+    <Stack>
+      <Box
+        px={2.8}
+        py={1.2}
+        borderBottom={1}
+        borderColor={(theme) => theme.palette.action.disabledBackground}
+        borderRadius={1}
+      >
+        <SearchChatForm />
+      </Box>
+      <Box sx={{ maxHeight: "70vh", overflow: "auto" }}>
+        <ChatList />
+      </Box>
+    </Stack>
+  );
 };
 
 export default MessageUsersSearch;
