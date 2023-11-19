f15dddd42a407e63b025aae0ce7d560bbd001d91
devcowsar <kawsarcse836@gmail.com>
2023-11-18 20:59:38 -0800

    feat: messageuserssearch

---
+++ src/widgets/MessageUsersSearch/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { MessageUsersSearchProps } from "./Types";
+
+const MessageUsersSearch: FC<MessageUsersSearchProps> = () => {
+  return <div>index</div>;
+};
+
+export default MessageUsersSearch;
