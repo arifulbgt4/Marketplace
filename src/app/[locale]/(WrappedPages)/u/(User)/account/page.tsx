cd7b5b02da829b0503624583663c61439741e650
jueal hossain <mdjueal920977@gmail.com>
2023-11-21 16:57:32 +0600

    feat: OwnerProfile

--- src/app/[locale]/(WrappedPages)/u/(User)/account/page.tsx
+++ src/app/[locale]/(WrappedPages)/u/(User)/account/page.tsx
@@ -4,13 +4,26 @@ import { Grid } from "@mui/material";
 import OwnerAbout from "src/widgets/OwnerAbout";
 import OwnerBasicInfo from "src/widgets/OwnerBasicInfo";
 import OwnerListings from "src/widgets/OwnerListings";
+import OwnerProfile from "src/widgets/OwnerProfile";
 import OwnerReview from "src/widgets/OwnerReview";
+
+const profileData = {
+  rating: 4.5,
+  totalListing: 8,
+  totalReview: 20,
+  cleanliness: 5,
+  communication: 3,
+  checkIn: 4,
+  accuracy: 3,
+  location: 2,
+  value: 5,
+};
 
 export default function Account() {
   return (
     <Grid container columnSpacing={10} rowSpacing={3}>
       <Grid item md={4} xs={12}>
-        Owner Profile
+        <OwnerProfile profileData={profileData} />
       </Grid>
       <Grid item container xs={8} rowSpacing={5}>
         <Grid item xs={12}>
---
+++ src/widgets/OwnerProfile/Types.ts
@@ -0,0 +1,13 @@
+export interface OwnerProfileProps {
+  profileData: {
+    rating: number;
+    totalListing: number;
+    totalReview: number;
+    cleanliness: number;
+    communication: number;
+    checkIn: number;
+    accuracy: number;
+    location: number;
+    value: number;
+  };
+}
