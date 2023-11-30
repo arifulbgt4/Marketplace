09934d3931fbef63a134046e9fc29e283d9661ed
devcowsar <kawsarcse836@gmail.com>
2023-11-30 07:57:37 -0800

    fix: question

--- src/widgets/Question/index.tsx
+++ src/widgets/Question/index.tsx
@@ -7,11 +7,19 @@ import { QuestionProps } from "./Types";
 const Question: FC<QuestionProps> = () => {
   return (
     <Container>
-      <Stack flexDirection="row" gap={5} justifyContent="center">
-        <Box px={15} py={5} bgcolor={(theme) => theme.palette.primary.main}>
+      <Stack
+        flexDirection={{ md: "row" }}
+        gap={{ md: 5, xs: 2 }}
+        justifyContent="center"
+      >
+        <Box
+          px={{ md: 15 }}
+          py={{ md: 5, xs: 3 }}
+          bgcolor={(theme) => theme.palette.primary.main}
+        >
           <Typography variant="h2">HAVE A QUESTION?</Typography>
         </Box>
-        <Box px={15} py={5} bgcolor="common.black">
+        <Box px={{ md: 15 }} py={{ md: 5, xs: 3 }} bgcolor="common.black">
           <Typography variant="h2" color="common.white">
             HAVE A QUESTION?
           </Typography>
