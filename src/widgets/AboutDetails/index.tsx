5ceea6360bf99c615f8a1f74d6f81e73d3021e85
devcowsar <kawsarcse836@gmail.com>
2023-12-01 02:50:05 -0800

    fix: aboutdetails

--- src/widgets/AboutDetails/index.tsx
+++ src/widgets/AboutDetails/index.tsx
@@ -39,7 +39,10 @@ const AboutDetails: FC<AboutDetailsProps> = () => {
               left={{ md: 0 }}
               top={{ md: 260 }}
             >
-              <Typography variant="h4">
+              <Typography
+                variant="h4"
+                textAlign={{ xs: "center", md: "initial" }}
+              >
                 Lorem ipsum dolor sit amet consectetur. Venenatis mauris purus
                 ornare porta id malesuada nibh vestibulum morbi. Quam blandit
                 scelerisque duis nunc erat lectus feugiat fames massa. Velit
@@ -49,8 +52,7 @@ const AboutDetails: FC<AboutDetailsProps> = () => {
             </Grid>
             <Grid item xs={12} md={5.5}>
               <CardMedia
-                sx={{ opacity: 0.7 }}
-                height={560}
+                sx={{ opacity: 0.7, height: { md: 560, xs: 300 } }}
                 component="img"
                 image="https://img.freepik.com/premium-photo/cooperation-action-group-young-modern-men-formalwear-working-using-computers-while-sitting-office_425904-830.jpg?w=1060"
               />
