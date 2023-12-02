93babb6e72c1b7c7868167414a5685993425159f
jueal hossain <mdjueal920977@gmail.com>
2023-12-02 19:28:58 +0600

    fix: UserLayout

--- src/layouts/UserLayout/index.tsx
+++ src/layouts/UserLayout/index.tsx
@@ -1,16 +1,7 @@
 "use client";
 import { FC } from "react";
 import { usePathname } from "next/navigation";
-import {
-  Grid,
-  Stack,
-  Button,
-  Avatar,
-  Typography,
-  Link,
-  Container,
-  Box,
-} from "@mui/material";
+import { Stack, Link, Container, Box } from "@mui/material";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import SettingsIcon from "@mui/icons-material/Settings";
@@ -28,103 +19,75 @@ const UserLayout: FC<UserLayoutProps> = ({ children }) => {
   const pathname = usePathname();
 
   return (
-    <Box pt={{ xs: 3, md: 8 }}>
-      <Container>
-        <Grid container>
-          <Grid item xs={12}>
-            <Stack
-              gap={{ xs: 3, md: 8 }}
-              direction="column"
-              justifyContent="space-between"
-              alignSelf="stretch"
+    <Box>
+      <Box py={2} bgcolor={(theme) => theme.palette.grey[100]}>
+        <Container>
+          <Stack
+            gap={{ xs: 3, md: 8 }}
+            direction="column"
+            justifyContent="space-between"
+            alignSelf="stretch"
+          >
+            <Tabs
+              value={
+                pathname.includes(routes.userSetting)
+                  ? routes.userSetting
+                  : pathname
+              }
             >
-              <Stack
-                flexDirection={{ md: "row" }}
-                justifyContent="space-between"
-                gap={{ xs: 2, md: 0 }}
-              >
-                <Stack gap={0.5}>
-                  <Stack direction="row" alignItems="center" gap={1}>
-                    <Avatar />
-                    <Typography variant="h4">User Name</Typography>
-                  </Stack>
-                  <Typography variant="subtitle1">
-                    member science, september,2023
-                  </Typography>
-                </Stack>
-                <Box width={{ xs: "100%", md: "auto" }}>
-                  <Button
-                    component={Link}
-                    href={`${routes.ListingCreatePage}`}
-                    variant="outlined"
-                    size="large"
-                    fullWidth
-                  >
-                    create Listing +
-                  </Button>
-                </Box>
-              </Stack>
-              <Tabs
-                value={
-                  pathname.includes(routes.userSetting)
-                    ? routes.userSetting
-                    : pathname
-                }
-              >
-                <Tab
-                  wrapped
-                  component={Link}
-                  href={routes.userDashboard}
-                  icon={<DashboardIcon fontSize="small" />}
-                  iconPosition="start"
-                  label="DASHBOARD"
-                  value={routes.userDashboard}
-                />
-                <Tab
-                  component={Link}
-                  href={routes.userAccount}
-                  icon={<ManageAccountsIcon fontSize="small" />}
-                  iconPosition="start"
-                  label="ACCOUNT"
-                  value={routes.userAccount}
-                />
-                <Tab
-                  component={Link}
-                  href={routes.userListing}
-                  icon={<ListAltIcon fontSize="small" />}
-                  iconPosition="start"
-                  label="LISTINGS"
-                  value={routes.userListing}
-                />
-                <Tab
-                  component={Link}
-                  href={routes.userBookmark}
-                  icon={<BookmarksIcon fontSize="small" />}
-                  iconPosition="start"
-                  label="BOOKMARKS"
-                  value={routes.userBookmark}
-                />
-                <Tab
-                  component={Link}
-                  href={routes.userOrder}
-                  icon={<ShoppingCartIcon fontSize="small" />}
-                  iconPosition="start"
-                  label="ORDER"
-                  value={routes.userOrder}
-                />
-                <Tab
-                  component={Link}
-                  href={routes.userSetting}
-                  icon={<SettingsIcon fontSize="small" />}
-                  iconPosition="start"
-                  label="SETTINGS"
-                  value={routes.userSetting}
-                />
-              </Tabs>
-            </Stack>
-          </Grid>
-        </Grid>
-      </Container>
+              <Tab
+                wrapped
+                component={Link}
+                href={routes.userDashboard}
+                icon={<DashboardIcon fontSize="small" />}
+                iconPosition="start"
+                label="DASHBOARD"
+                value={routes.userDashboard}
+              />
+              <Tab
+                component={Link}
+                href={routes.userAccount}
+                icon={<ManageAccountsIcon fontSize="small" />}
+                iconPosition="start"
+                label="ACCOUNT"
+                value={routes.userAccount}
+              />
+              <Tab
+                component={Link}
+                href={routes.userListing}
+                icon={<ListAltIcon fontSize="small" />}
+                iconPosition="start"
+                label="LISTINGS"
+                value={routes.userListing}
+              />
+              <Tab
+                component={Link}
+                href={routes.userBookmark}
+                icon={<BookmarksIcon fontSize="small" />}
+                iconPosition="start"
+                label="BOOKMARKS"
+                value={routes.userBookmark}
+              />
+              <Tab
+                component={Link}
+                href={routes.userOrder}
+                icon={<ShoppingCartIcon fontSize="small" />}
+                iconPosition="start"
+                label="ORDER"
+                value={routes.userOrder}
+              />
+              <Tab
+                component={Link}
+                href={routes.userSetting}
+                icon={<SettingsIcon fontSize="small" />}
+                iconPosition="start"
+                label="SETTINGS"
+                value={routes.userSetting}
+              />
+            </Tabs>
+          </Stack>
+        </Container>
+      </Box>
       <Container>
         <Box pt={{ xs: 3, md: 5 }}>{children}</Box>
       </Container>
