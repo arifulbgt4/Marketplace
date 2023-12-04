4d2dacdca59d5976aa2cbe48197422d6fdde4a74
devcowsar <kawsarcse836@gmail.com>
2023-12-04 08:42:08 -0800

    fix: Listingheader

--- src/widgets/ListingHeader/index.tsx
+++ src/widgets/ListingHeader/index.tsx
@@ -1,8 +1,21 @@
-import { FC } from "react";
-import { Hidden, Rating, Stack, Typography } from "@mui/material";
+"use client";
+import { FC, useState } from "react";
+import {
+  Hidden,
+  Rating,
+  Stack,
+  Typography,
+  Button,
+  Box,
+  IconButton,
+} from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
 
 import { ListingHeaderProps } from "./Types";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+import ShareIcon from "@mui/icons-material/Share";
+import { Divider } from "@mui/material";
 
 const ListingHeader: FC<ListingHeaderProps> = ({
   title,
@@ -11,9 +24,41 @@ const ListingHeader: FC<ListingHeaderProps> = ({
   creator,
   address,
 }) => {
+  const [isSelect, setIsSelect] = useState(false);
   return (
     <Stack gap={1}>
-      <Typography variant="h4">{title}</Typography>
+      <Stack direction="row" justifyContent="space-between">
+        <Box>
+          <Typography variant="h4">{title}</Typography>
+        </Box>
+        <Box
+          borderRadius={5}
+          border={1}
+          borderColor={(theme) => theme.palette.action.active}
+          display="flex"
+          flexDirection="row"
+        >
+          <Button
+            onClick={() => {
+              setIsSelect((prv) => !prv);
+            }}
+            size="large"
+          >
+            {isSelect ? (
+              <BookmarkIcon color="primary" />
+            ) : (
+              <BookmarkBorderOutlinedIcon color="primary" />
+            )}
+          </Button>
+          <Divider
+            orientation="vertical"
+            sx={{ color: (theme) => theme.palette.divider }}
+          />
+          <Button>
+            <ShareIcon />
+          </Button>
+        </Box>
+      </Stack>
       <Stack flexDirection="row" alignItems="center" gap={1}>
         {rating && (
           <>
