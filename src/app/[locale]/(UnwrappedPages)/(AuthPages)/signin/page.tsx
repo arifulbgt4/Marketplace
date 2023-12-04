fef9c693c434c61fe37c53a9ff7c41d047fad680
jueal hossain <mdjueal920977@gmail.com>
2023-12-04 22:09:46 +0600

    fix: sign in page

--- src/app/[locale]/(UnwrappedPages)/(AuthPages)/signin/page.tsx
+++ src/app/[locale]/(UnwrappedPages)/(AuthPages)/signin/page.tsx
@@ -1,19 +1,13 @@
-import { Grid, Container, Box } from "@mui/material";
+import { Container, Stack } from "@mui/material";
 
 import SigninForm from "src/forms/SigninForm";
-import SigninInfo from "src/widgets/SigninInfo";
 
 export default function SignIn() {
   return (
     <Container>
-      <Grid container columnSpacing={8} rowSpacing={5}>
-        <Grid item sm={12} md={8}>
-          <SigninForm />
-        </Grid>
-        <Grid item sm={12} md={4}>
-          <SigninInfo />
-        </Grid>
-      </Grid>
+      <Stack alignItems="center" justifyContent="center" height="100vh">
+        <SigninForm />
+      </Stack>
     </Container>
   );
 }
--- src/forms/SigninForm/index.tsx
+++ src/forms/SigninForm/index.tsx
@@ -1,9 +1,18 @@
 "use client";
 // React
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 // @mui
-import { Box, Typography, Button, Grid } from "@mui/material";
+import {
+  Typography,
+  Button,
+  Grid,
+  Stack,
+  Divider,
+  IconButton,
+} from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 // packages
 import { Form as FinalForm } from "react-final-form";
@@ -15,6 +24,9 @@ import { UserSigninOptions } from "src/global/types";
 import { signIn } from "./actions";
 
 import { SiginFormProps } from "./Types";
+import Logo from "src/components/Logo";
+import { Facebook, FacebookRounded, Google } from "@mui/icons-material";
+import routes from "src/global/routes";
 
 const INITIAL_VALUES: Omit<UserSigninOptions, "callbackUrl"> = {
   email: "",
@@ -22,6 +34,7 @@ const INITIAL_VALUES: Omit<UserSigninOptions, "callbackUrl"> = {
 };
 
 const SigninForm: FC<SiginFormProps> = () => {
+  const [isShow, setIsShow] = useState(false);
   const router = useRouter();
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl") || "/";
@@ -40,77 +53,131 @@ const SigninForm: FC<SiginFormProps> = () => {
     }
   };
 
-  return (
-    <Grid container>
-      <Grid item xs={12} md={8}>
-        <Box
-          sx={(theme) => ({
-            borderRadius: 2,
-            [theme.breakpoints.down("md")]: {
-              px: 0,
-            },
-          })}
-        >
-          <Typography variant="h5" sx={{ mb: 4 }}>
-            Login
-          </Typography>
-          <FinalForm
-            onSubmit={onSubmitForm}
-            initialValues={INITIAL_VALUES}
-            render={({ handleSubmit, values, errors, submitting }) => {
-              return (
-                <form onSubmit={handleSubmit}>
-                  <TextField
-                    id="email"
-                    name="email"
-                    label="Email"
-                    required
-                    size="small"
-                    type="email"
-                    fullWidth
-                    fieldProps={{
-                      validate: composeValidators(required("Email required")),
-                    }}
-                    sx={{
-                      mb: 2.5,
-                    }}
-                  />
-                  <TextField
-                    id="password"
-                    name="password"
-                    label="Password"
-                    required
-                    size="small"
-                    fullWidth
-                    type="password"
-                    fieldProps={{
-                      validate: composeValidators(
-                        required("Password required")
-                      ),
-                    }}
-                    sx={{
-                      mb: 2.5,
-                    }}
-                  />
+  const handlevisibility = () => setIsShow((show) => !show);
 
+  return (
+    <Stack width={500} gap={2.5}>
+      <Stack border={1} borderColor="divider" gap={2.5} sx={{ p: 5 }}>
+        <Stack justifyContent="center" alignItems="center" pb={2.5}>
+          <Logo />
+        </Stack>
+        <Divider />
+        <Typography textAlign="center" variant="h3">
+          Sign In
+        </Typography>
+        <FinalForm
+          onSubmit={onSubmitForm}
+          initialValues={INITIAL_VALUES}
+          render={({ handleSubmit, values, errors, submitting }) => {
+            return (
+              <form onSubmit={handleSubmit}>
+                <Stack gap={2.5}>
+                  <Stack gap={2}>
+                    <TextField
+                      id="email"
+                      name="email"
+                      label="Mobile Number, Username or Email"
+                      required
+                      size="small"
+                      type="email"
+                      fullWidth
+                      fieldProps={{
+                        validate: composeValidators(required("Email required")),
+                      }}
+                    />
+                    <TextField
+                      id="password"
+                      name="password"
+                      label="Password"
+                      required
+                      size="small"
+                      fullWidth
+                      type={isShow ? "text" : "password"}
+                      fieldProps={{
+                        validate: composeValidators(
+                          required("Password required")
+                        ),
+                      }}
+                      InputProps={{
+                        endAdornment: (
+                          <IconButton onClick={handlevisibility}>
+                            {isShow ? (
+                              <VisibilityOffIcon />
+                            ) : (
+                              <VisibilityIcon />
+                            )}
+                          </IconButton>
+                        ),
+                      }}
+                    />
+                  </Stack>
                   <Button
                     type="submit"
                     variant="contained"
                     color="info"
                     disabled={submitting}
+                    size="large"
                   >
-                    Submit
+                    sign in
                   </Button>
-                </form>
-              );
-            }}
-          />
-        </Box>
-      </Grid>
-    </Grid>
+                  <Grid
+                    container
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    <Grid item xs={5.5}>
+                      <Divider />
+                    </Grid>
+                    <Grid
+                      item
+                      xs={1}
+                      justifyContent="center"
+                      alignItems="center"
+                      display="flex"
+                    >
+                      <Typography variant="subtitle2" color="text.secondary">
+                        or
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={5.5}>
+                      <Divider />
+                    </Grid>
+                  </Grid>
+                  <Stack
+                    flexDirection="row"
+                    gap={5}
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    <IconButton>
+                      <Google fontSize="large" />
+                    </IconButton>
+                    <IconButton>
+                      <FacebookRounded fontSize="large" color="primary" />
+                    </IconButton>
+                  </Stack>
+                  <Button size="large">Forgot password?</Button>
+                </Stack>
+              </form>
+            );
+          }}
+        />
+      </Stack>
+      <Stack
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+        py={5}
+        border={1}
+        borderColor="divider"
+      >
+        <Typography variant="subtitle1">Have an account?</Typography>
+        <Button href={routes.signup}>Sign up</Button>
+      </Stack>
+    </Stack>
   );
 };
 
-// Exports
 export * from "./Types";
 export default SigninForm;
