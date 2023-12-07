66a12314552b04cac9ae40811e485d0eeae56060
devcowsar <kawsarcse836@gmail.com>
2023-12-07 06:33:00 -0800

    fix: aboutpagelinkprops

--- src/widgets/Question/index.tsx
+++ src/widgets/Question/index.tsx
@@ -14,55 +14,56 @@ const Question: FC<QuestionProps> = () => {
         gap={{ md: 5, xs: 2 }}
         justifyContent="center"
       >
-        <Link href={routes.faq}>
-          <Box
-            px={{ md: 15 }}
-            py={{ md: 12, xs: 2 }}
-            bgcolor="info.light"
-            color="info.contrastText"
-            textAlign="center"
+        <Box
+          component={Link}
+          href={routes.faq}
+          px={{ md: 15 }}
+          py={{ md: 12, xs: 2 }}
+          bgcolor="info.light"
+          color="info.contrastText"
+          textAlign="center"
+          fontWeight={100}
+          sx={(theme) => ({
+            transition: ".5s",
+            "&:hover": {
+              bgcolor: "info.main",
+              transform: "scale(1.02)",
+              borderRadius: 1,
+              boxShadow: theme.shadows[1],
+            },
+          })}
+        >
+          <Typography sx={{ typography: { md: "h2", xs: "h3" } }}>
+            HAVE A QUESTION?
+          </Typography>
+        </Box>
+
+        <Box
+          component={Link}
+          href={routes.search}
+          px={{ md: 15 }}
+          py={{ md: 12, xs: 2 }}
+          bgcolor="secondary.main"
+          textAlign="center"
+          sx={{
+            transition: ".5s",
+            "&:hover": {
+              bgcolor: "secondary.dark",
+              transform: "scale(1.02)",
+              borderRadius: 1,
+              boxShadow: 1,
+            },
+          }}
+          color="secondary.contrastText"
+        >
+          <Typography
             fontWeight={100}
-            sx={(theme) => ({
-              transition: ".5s",
-              "&:hover": {
-                bgcolor: "info.main",
-                transform: "scale(1.02)",
-                borderRadius: 1,
-                boxShadow: theme.shadows[1],
-              },
-            })}
+            sx={{ typography: { md: "h2", xs: "h3" } }}
+            color="common.white"
           >
-            <Typography sx={{ typography: { md: "h2", xs: "h3" } }}>
-              HAVE A QUESTION?
-            </Typography>
-          </Box>
-        </Link>
-        <Link href={routes.search}>
-          <Box
-            px={{ md: 15 }}
-            py={{ md: 12, xs: 2 }}
-            bgcolor="secondary.main"
-            textAlign="center"
-            sx={{
-              transition: ".5s",
-              "&:hover": {
-                bgcolor: "secondary.dark",
-                transform: "scale(1.02)",
-                borderRadius: 1,
-                boxShadow: 1,
-              },
-            }}
-            color="secondary.contrastText"
-          >
-            <Typography
-              fontWeight={100}
-              sx={{ typography: { md: "h2", xs: "h3" } }}
-              color="common.white"
-            >
-              FIND AN AGENT
-            </Typography>
-          </Box>
-        </Link>
+            FIND AN AGENT
+          </Typography>
+        </Box>
       </Stack>
     </Container>
   );
