702fadabc3d4a9d2da0a329df49e3d55feb0e150
Ariful islam <arifulbgt4@gmail.com>
2024-01-31 19:35:13 +0600

    feat: vercel speed insights activation

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata, Viewport } from "next";
 import { useLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { siteConfig } from "src/global/config";
 import NextAuthProvider from "src/layouts/NextAuthProvider";
@@ -76,6 +77,7 @@ export default function RootLayout({
         <body suppressHydrationWarning={true}>
           <NextAuthProvider>{children}</NextAuthProvider>
           <Analytics />
+          <SpeedInsights />
         </body>
       </ThemeContextProvider>
     </html>
