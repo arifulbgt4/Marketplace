f1cd57e31733d8d51ebe8fcabe81bbf80258b7c8
jueal hossain <juealdev@gmail.com>
2024-02-10 00:15:32 +0600

    fix: user log in

--- src/widgets/UserLogIn/index.tsx
+++ src/widgets/UserLogIn/index.tsx
@@ -4,11 +4,15 @@ import {
   Link,
   ListItem,
   ListItemButton,
+  ListItemIcon,
   ListItemText,
   Menu,
+  Paper,
   Stack,
 } from "@mui/material";
 import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import LoginIcon from "@mui/icons-material/Login";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
 
 import routes from "src/global/routes";
 
@@ -37,19 +41,22 @@ const UserLogIn: FC<UserLogInProps> = () => {
         />
       </IconButton>
       <Menu
-        elevation={0}
+        elevation={24}
         sx={(theme) => ({
           mt: { xs: -4.7, md: 7.9 },
           right: { xs: -16, md: 0 },
-          left: { xs: -16, md: 0 },
+          left: { xs: -16, md: 3 },
           mb: { xs: 7, md: 0 },
           background: "transparent",
           backdropFilter: "blur(2px)",
           "& .MuiMenu-paper": {
-            borderTopRightRadius: { xs: 24, md: 4 },
-            borderTopLeftRadius: { xs: 24, md: 4 },
+            borderTopRightRadius: { xs: 24, md: 0 },
+            borderTopLeftRadius: { xs: 24, md: 0 },
+            borderBottomRightRadius: { xs: 0, md: 18 },
+            borderBottomLeftRadius: { xs: 0, md: 18 },
             boxShadow: { xs: 15, md: 4 },
             top: { md: "0px !important" },
+            bgcolor: "Background.paper",
           },
         })}
         id="menu-appbar"
@@ -66,7 +73,7 @@ const UserLogIn: FC<UserLogInProps> = () => {
         open={Boolean(anchorElNav)}
         onClose={handleCloseNavMenu}
       >
-        <Stack width={{ xs: "100vw", md: "auto" }}>
+        <Stack width={{ xs: "100vw", md: 320 }} height={{ md: 150 }}>
           <ListItem
             disablePadding
             component={Link}
@@ -74,8 +81,10 @@ const UserLogIn: FC<UserLogInProps> = () => {
             href={routes.signin}
           >
             <ListItemButton sx={{ pr: 3 }}>
+              <LoginIcon sx={{ color: "text.secondary" }} />
+
               <ListItemText
-                sx={{ textAlign: "end", color: "text.secondary" }}
+                sx={{ color: "text.secondary", pl: 2, fontWeight: 600 }}
                 primary="Sign In"
               />
             </ListItemButton>
@@ -87,8 +96,10 @@ const UserLogIn: FC<UserLogInProps> = () => {
             href={routes.signup}
           >
             <ListItemButton>
+              <PersonAddIcon sx={{ color: "text.secondary" }} />
+
               <ListItemText
-                sx={{ textAlign: "end", color: "text.secondary" }}
+                sx={{ color: "text.secondary", pl: 2, fontWeight: 600 }}
                 primary=" Sign Up"
               />
             </ListItemButton>
