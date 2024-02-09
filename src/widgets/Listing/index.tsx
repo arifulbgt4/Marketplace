ddc6e667b32849e2a6872a572fe96ba558774055
jueal hossain <juealdev@gmail.com>
2024-02-09 20:01:24 +0600

    Revert "fix: listings design"
    This reverts commit 1a1465fada803f42304d824079e04c5ee8abfbf0.

--- src/widgets/Listing/index.tsx
+++ src/widgets/Listing/index.tsx
@@ -1,14 +1,24 @@
 "use client";
 import { FC, useState } from "react";
 import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
   CardMedia,
   Typography,
   Link,
   Stack,
   IconButton,
+  Grid,
   Box,
   Avatar,
 } from "@mui/material";
+import BedIcon from "@mui/icons-material/Bed";
+import BathtubIcon from "@mui/icons-material/Bathtub";
+import DeselectIcon from "@mui/icons-material/Deselect";
+import ApartmentIcon from "@mui/icons-material/Apartment";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
 import CategoryIcon from "@mui/icons-material/Category";
@@ -35,36 +45,28 @@ const Listing: FC<ListingProps> = ({
   const [show, setShow] = useState(false);
 
   return (
-    <Stack
-      // elevation={10}
-      onMouseOver={() => setShow(true)}
-      onMouseOut={() => setShow(false)}
+    <Card
+      elevation={24}
       sx={{
+        display: "flex",
         flexDirection: isGrid ? "column" : "row",
         gap: isGrid ? 0 : 3,
+        position: "relative",
         borderRadius: 3,
-        // bgcolor: "transparent",
-        ":hover": {
-          boxShadow: 14,
-        },
+        bgcolor: "transparent",
       }}
     >
       <Box component={Link} href={`${routes.listingDetails}/${slug}`}>
         <CardMedia
-          sx={{
-            borderRadius: 3,
-
-            ":hover": {
-              borderBottomRightRadius: !show ? 12 : 0,
-              borderBottomLeftRadius: !show ? 12 : 0,
-            },
-          }}
           image={image}
+          onMouseOver={() => setShow(true)}
+          onMouseOut={() => setShow(false)}
         >
           <Stack height={isGrid ? 250 : 223}>
             {show && (
               <Stack
-                p={2}
+                px={1}
+                pb={2}
                 justifyContent="space-between"
                 alignItems="end"
                 flexDirection="row"
@@ -103,42 +105,123 @@ const Listing: FC<ListingProps> = ({
           </Stack>
         </CardMedia>
       </Box>
-      <Stack
-        p={2}
-        width="100%"
-        flexDirection="row"
-        justifyContent="space-between"
-      >
-        <Stack
-          flexDirection="row"
-          alignItems="center"
-          gap={1}
-          component={Link}
-          color="text.primary"
-          href={`${routes.listingDetails}/${slug}`}
-          sx={{
-            "&:hover": {
-              color: "primary.main",
-            },
-          }}
-        >
-          <Avatar sx={{ height: 24, width: 24 }} />
-          <Typography variant="h6">Kamruzzaman</Typography>
-        </Stack>
-        <Stack
-          flexDirection="row"
-          justifyContent="center"
-          alignItems="center"
-          gap={0.5}
-          color="text.secondary"
-        >
-          <CategoryIcon sx={{ height: 18, width: 18 }} />
-          <Typography fontWeight={600} variant="caption">
-            2.5
-          </Typography>
-        </Stack>
+
+      <Stack p={isGrid ? 0 : 1} width="100%" justifyContent="space-between">
+        <CardHeader
+          action={
+            <Stack
+              flexDirection="row"
+              justifyContent="center"
+              alignItems="center"
+              gap={0.5}
+              color="text.secondary"
+              mt={1}
+            >
+              <CategoryIcon sx={{ height: 18, width: 18 }} />
+              <Typography fontWeight={600} variant="caption">
+                2.5
+              </Typography>
+            </Stack>
+          }
+          title={
+            <Stack
+              flexDirection="row"
+              // justifyContent="center"
+              alignItems="center"
+              gap={1}
+              component={Link}
+              color="text.primary"
+              href={`${routes.listingDetails}/${slug}`}
+              sx={{
+                "&:hover": {
+                  color: "primary.main",
+                },
+              }}
+            >
+              <Avatar sx={{ height: 24, width: 24 }} />
+              <Typography variant="h6">Kamruzzaman</Typography>
+            </Stack>
+          }
+        />
+        {/* {services && (
+          <CardContent>
+            <Grid container rowSpacing={2}>
+              <Grid item xs={6}>
+                <Stack flexDirection="row" gap={2} alignItems="center">
+                  <IconButton>
+                    <BedIcon fontSize="small" />
+                  </IconButton>
+                  <Typography color="text.secondary" variant="subtitle2">
+                    {services?.bed} Beds
+                  </Typography>
+                </Stack>
+              </Grid>
+              <Grid item xs={6}>
+                <Stack
+                  flexDirection="row"
+                  gap={2}
+                  alignItems="center"
+                  justifyContent="flex-end"
+                >
+                  <IconButton>
+                    <BathtubIcon fontSize="small" />
+                  </IconButton>
+                  <Typography color="text.secondary" variant="subtitle2">
+                    {services?.bath} Bath
+                  </Typography>
+                </Stack>
+              </Grid>
+              <Grid item xs={6}>
+                <Stack flexDirection="row" gap={2} alignItems="center">
+                  <IconButton>
+                    <DeselectIcon fontSize="small" />
+                  </IconButton>
+                  <Typography color="text.secondary" variant="subtitle2">
+                    {services?.area} Sqft
+                  </Typography>
+                </Stack>
+              </Grid>
+              <Grid item xs={6}>
+                <Stack
+                  flexDirection="row"
+                  gap={2}
+                  alignItems="center"
+                  justifyContent="flex-end"
+                >
+                  <IconButton>
+                    <ApartmentIcon fontSize="small" />
+                  </IconButton>
+                  <Typography color="text.secondary" variant="subtitle2">
+                    {services?.apartment} Apartment
+                  </Typography>
+                </Stack>
+              </Grid>
+            </Grid>
+          </CardContent>
+        )} */}
       </Stack>
-    </Stack>
+      {/* {price && (
+        <CardActions>
+          <Stack
+            height="100%"
+            flexDirection={isGrid ? "row" : "column"}
+            justifyContent="space-between"
+            flex={1}
+            padding={isGrid ? 0 : 2}
+            pb={isGrid ? 0 : 3}
+          >
+            <Typography variant="h5">{price}/mo</Typography>
+            <Button
+              variant="outlined"
+              component={Link}
+              href={`${routes.listingDetails}/${slug}`}
+            >
+              view details
+            </Button>
+          </Stack>
+        </CardActions>
+      )} */}
+    </Card>
   );
 };
 
