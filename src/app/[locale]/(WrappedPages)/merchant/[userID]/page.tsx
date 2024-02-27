01d672283878bb94c66b4e281ce8faaaa7af6ef4
jueal hossain <juealdev@gmail.com>
2024-02-28 01:03:06 +0600

    fix

--- src/app/[locale]/(WrappedPages)/merchant/[userID]/page.tsx
+++ src/app/[locale]/(WrappedPages)/merchant/[userID]/page.tsx
@@ -8,8 +8,8 @@ const HostProfile = () => {
   return (
     <Box>
       <CardMedia
-        sx={{ height: { xs: 182, md: 236 } }}
-        src="https://www.w3schools.com/howto/img_avatar.png"
+        sx={{ height: { xs: 182, md: 276 } }}
+        src="https://cdn.pixabay.com/photo/2015/01/08/18/27/startup-593341_1280.jpg"
         component="img"
         alt="JL"
       />
